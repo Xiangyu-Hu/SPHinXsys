3404cab450fa5aa220cbf1d9ced4118f9e19b0cc
Ubuntu <virtonomy@SPHINXJS.qg4qjrxycqxetcothedhm1ylih.ax.internal.cloudapp.net>
2021-02-02 15:00:41 +0000

    oneTBB source added

--- submodules/copy_to_source.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-shutil.copytree('oneTBB/src', '../SPHINXsys/src/shared/oneTBB')
-shutil.copytree('simbody/Simbody', '../SPHINXsys/src/shared/Simbody')
-shutil.copytree('simbody/SimTKcommon', '../SPHINXsys/src/shared/SimTKcommon')
-shutil.copytree('simbody/SimTKmath', '../SPHINXsys/src/shared/SimTKmath')
-
-#find . -mindepth 0 -type d -exec cp ../CMakeLists.txt {} \;
