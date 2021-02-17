cb8df3a1eef369aedc2a35398f41ffe05c4983c3
Ubuntu <virtonomy@SPHYNXJS-D32v4.qg4qjrxycqxetcothedhm1ylih.ax.internal.cloudapp.net>
2021-02-17 18:17:26 +0000

    c-maes references fixed

--- cmake/ImportSPHINXsysFromSource_for_3D_build.cmake
+++ cmake/ImportSPHINXsysFromSource_for_3D_build.cmake
@@ -1,5 +1,4 @@
 FILE(GLOB_RECURSE SPHINXsysHeaderPathListShared ${CMAKE_SOURCE_DIR}/SPHINXsys/src/shared/*.h)
-FILE(GLOB_RECURSE SPHINXsysHeaderPathListOneTBB ${CMAKE_SOURCE_DIR}/SPHINXsys/src/oneTBB/*.h)
 FILE(GLOB_RECURSE SPHINXsysHeaderPathListFor3DBuild ${CMAKE_SOURCE_DIR}/SPHINXsys/src/for_3D_build/*.h)
 FILE(GLOB_RECURSE SPHINXsysHeaderPathListFor3DBuildHpp ${CMAKE_SOURCE_DIR}/SPHINXsys/src/for_3D_build/*.hpp)
 
