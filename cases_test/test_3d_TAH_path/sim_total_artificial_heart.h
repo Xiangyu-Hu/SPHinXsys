f15f74d67c9c7d5571441cf4cb454fa914b10d30
wenyang.chu <chu@virtonomy.io>
2021-06-19 15:28:21 -0400

    SimTotalArtificialHeartInput input; use  in main

--- cases_test/test_3d_TAH_path/sim_total_artificial_heart.h
+++ cases_test/test_3d_TAH_path/sim_total_artificial_heart.h
@@ -12,112 +12,72 @@ using namespace SPH;
 
 struct SimTotalArtificialHeartInput
 {
-public:
-	Real scale_stl;
-	Real resolution_tah;
-	Real resolution_aorta;
-	Real resolution_diaphragm;
-	Real resolution_latrium;
-	Real resolution_partery;
-	Real resolution_ratrium;
-	Real rho_0;
-	Real poisson;
-	Real Youngs_modulus;
-	Real Youngs_modulus_tah;
-	Real physical_viscosity;
-	std::array<Real, 3> translation_tah;
+	double scale_stl;
+	double resolution_tah;
+	double resolution_aorta;
+	double resolution_diaphragm;
+	double resolution_latrium;
+	double resolution_partery;
+	double resolution_ratrium;
+	double rho_0;
+	double poisson;
+	double Youngs_modulus;
+	double Youngs_modulus_tah;
+	double physical_viscosity;
+	std::array<double, 3> translation_tah;
 	std::vector<std::string> stls;
 	std::string relative_input_path;
-
-	SimTotalArtificialHeartInput(
-		Real scale_stl,
-		Real resolution_tah,
-		Real resolution_aorta,
-		Real resolution_diaphragm,
-		Real resolution_latrium,
-		Real resolution_partery,
-		Real resolution_ratrium,
-		Real rho_0,
-		Real poisson,
-		Real Youngs_modulus,
-		Real Youngs_modulus_tah,
-		Real physical_viscosity,
-		std::array<Real, 3> translation_tah,
-		std::vector<std::string> stls,
-		std::string relative_input_path);
+	std::vector<std::pair<int, int>> contacting_bodies_list;
 };
 
 class SimTotalArtificialHeart
 {
 public:
-	SimTotalArtificialHeart()
+	SimTotalArtificialHeart(SimTotalArtificialHeartInput &input)
 	{
-		//TODO set up struct, move below values out of constructor
+		std::vector<string> imported_stl_list(std::begin(input.stls), std::end(input.stls));
+		std::vector<Vec3d> translation_list = {Vec3d(input.translation_tah[0], input.translation_tah[1],
+													 input.translation_tah[2]),
+											   Vec3d(0), Vec3d(0), Vec3d(0), Vec3d(0), Vec3d(0)};
+		std::vector<Real> resolution_list = {input.resolution_tah, input.resolution_aorta, input.resolution_diaphragm,
+											 input.resolution_latrium,
+											 input.resolution_partery, input.resolution_ratrium};
 
-		/** INPUT PARAMETERS */
-		Real scale_stl = 0.001;
-		Real resolution_tah = 8;
-		Real resolution_aorta = 8;
-		Real resolution_diaphragm = 8;
-		Real resolution_latrium = 8;
-		Real resolution_partery = 8;
-		Real resolution_ratrium = 8;
-		Real rho_0 = 1000.0;
-		Real poisson = 0.35;
-		Real Youngs_modulus = 1e5;
-		Real Youngs_modulus_tah = 1e6;
-		Real physical_viscosity = 200;
-		Real translation_tah[] = {0, -200, 0};
-		std::string stls[] = {"TAH_basic2_pos.stl", "Aorta.stl", "Diaphragm.stl", "LA.stl", "PA.stl", "RA.stl"};
-		string relative_input_path = "./input/";
-
-		vector<string> imported_stl_list(std::begin(stls), std::end(stls));
-		vector<Vec3d> translation_list = {Vec3d(translation_tah[0], translation_tah[1], translation_tah[2]), Vec3d(0), Vec3d(0), Vec3d(0), Vec3d(0), Vec3d(0)};
-		vector<Real> resolution_list = {resolution_tah, resolution_aorta, resolution_diaphragm, resolution_latrium,
-										resolution_partery, resolution_ratrium};
-
-		LinearElasticSolid material_tah = LinearElasticSolid(rho_0, Youngs_modulus_tah, poisson);
-		NeoHookeanSolid material_vessel = NeoHookeanSolid(rho_0, Youngs_modulus, poisson);
-		vector<LinearElasticSolid> material_model_list = {material_tah,
-														  material_vessel, material_vessel, material_vessel, material_vessel, material_vessel};
-
-		vector<IndexPair> contacting_bodies_list = {IndexPair(0, 1), IndexPair(0, 2), IndexPair(0, 3), IndexPair(0, 4),
-													IndexPair(0, 5), IndexPair(1, 4), IndexPair(3, 4), IndexPair(4, 5)};
-		/** CONTACT ORGANS WITH ORGANS*/
-		//IndexPair(1, 4); //Aorta with PA
-		//IndexPair(2, 5); //Diaphragm with RA
-		//IndexPair(3, 4); //LA with PA
-		//IndexPair(4, 5); //PA with RA
+		LinearElasticSolid material_tah = LinearElasticSolid(input.rho_0, input.Youngs_modulus_tah, input.poisson);
+		NeoHookeanSolid material_vessel = NeoHookeanSolid(input.rho_0, input.Youngs_modulus, input.poisson);
+		std::vector<LinearElasticSolid> material_model_list = {material_tah,
+															   material_vessel, material_vessel, material_vessel,
+															   material_vessel, material_vessel};
 
 		/** INPUT DECLERATION */
-		StructuralSimulationInput input{
-			relative_input_path,
+		StructuralSimulationInput inputStructuralSim{
+			input.relative_input_path,
 			imported_stl_list,
-			scale_stl,
+			input.scale_stl,
 			translation_list,
 			resolution_list,
 			material_model_list,
-			physical_viscosity,
-			contacting_bodies_list};
-		input.non_zero_gravity_ = vector<GravityPair>{GravityPair(0, Vec3d(0.0, 45.0, 0.0))}; // gravity for TAH
-		input.spring_damper_tuple_ = {SpringDamperTuple(1, Vec3d(0.1, 0.1, 0.1), 0.01),
-									  SpringDamperTuple(2, Vec3d(0.1, 0.1, 0.1), 0.01),
-									  SpringDamperTuple(3, Vec3d(0.1, 0.1, 0.1), 0.01),
-									  SpringDamperTuple(4, Vec3d(0.1, 0.1, 0.1), 0.01),
-									  SpringDamperTuple(5, Vec3d(0.1, 0.1, 0.1), 0.01)};
+			input.physical_viscosity,
+			input.contacting_bodies_list};
+		inputStructuralSim.non_zero_gravity_ = std::vector<GravityPair>{GravityPair(0, Vec3d(0.0, 45.0, 0.0))}; // gravity for TAH
+		inputStructuralSim.spring_damper_tuple_ = {SpringDamperTuple(1, Vec3d(0.1, 0.1, 0.1), 0.01),
+												   SpringDamperTuple(2, Vec3d(0.1, 0.1, 0.1), 0.01),
+												   SpringDamperTuple(3, Vec3d(0.1, 0.1, 0.1), 0.01),
+												   SpringDamperTuple(4, Vec3d(0.1, 0.1, 0.1), 0.01),
+												   SpringDamperTuple(5, Vec3d(0.1, 0.1, 0.1), 0.01)};
 
 		/** SIMULATION MODEL */
-		sim.reset(new StructuralSimulation(&input));
+		sim.reset(new StructuralSimulation(&inputStructuralSim));
 	};
 
 	~SimTotalArtificialHeart(){};
 
 public: //C++ Backend functions
-	void initAndRunCompleteSimulation(float endTime) { sim->RunSimulation(SPH::Real(endTime)); };
+	void initAndRunCompleteSimulation(double endTime) { sim->RunSimulation(SPH::Real(endTime)); };
 
 public: //WASM functions
 	void initSimulationJS() { sim->InitSimulationJS(); };
-	void runSimulationFixedDurationJS(float duration) { sim->RunSimulationFixedDurationJS(SPH::Real(duration)); };
+	void runSimulationFixedDurationJS(double duration) { sim->RunSimulationFixedDurationJS(SPH::Real(duration)); };
 
 private:
 	std::unique_ptr<StructuralSimulation> sim;
--- cases_test/test_3d_TAH_path/total_artificial_heart.cpp
+++ cases_test/test_3d_TAH_path/total_artificial_heart.cpp
@@ -9,13 +9,40 @@ int main(int argc, char *argv[])
 {
 	//use simulation collection id to get json file for simulation definittion
 	string ticket_id = argv[0];
+	//download JSON file and fill in the following struct	
 
-	//download STL file here for c++ application
+	SimTotalArtificialHeartInput input;
+	input.scale_stl = 0.001;
+	input.resolution_tah = 8.0;
+	input.resolution_aorta = 8.0;
+	input.resolution_diaphragm = 8.0;
+	input.resolution_latrium = 8.0;
+	input.resolution_partery = 8.0;
+	input.resolution_ratrium = 8.0;
+	input.rho_0 = 1000.0;
+	input.poisson = 0.35;
+	input.Youngs_modulus = 1e5;
+	input.Youngs_modulus_tah = 1e6;
+	input.physical_viscosity = 200.0;
+	input.translation_tah = {0, -200.0, 0};
+	input.stls = {"TAH_basic2_pos.stl", "Aorta.stl", "Diaphragm.stl", "LA.stl", "PA.stl", "RA.stl"};
+	input.relative_input_path = "./input/";
+	input.contacting_bodies_list = {std::pair<int, int>(0, 1), std::pair<int, int>(0, 2), std::pair<int, int>(0, 3),
+									std::pair<int, int>(0, 4), std::pair<int, int>(0, 5), std::pair<int, int>(1, 4),
+									std::pair<int, int>(3, 4), std::pair<int, int>(4, 5)};
+	/** CONTACT ORGANS WITH ORGANS*/
+	//IndexPair(1, 4); //Aorta with PA
+	//IndexPair(2, 5); //Diaphragm with RA
+	//IndexPair(3, 4); //LA with PA
+	//IndexPair(4, 5); //PA with RA
 
-	SimTotalArtificialHeart simTotalArtificialHeart;
+	//download STLs file
+	
+	SimTotalArtificialHeart simTotalArtificialHeart(input);
 	simTotalArtificialHeart.initSimulationJS();
-	for (int step=0;step<5;step++){
-		simTotalArtificialHeart.runSimulationFixedDurationJS(0.1*step);
+	for (int step = 0; step < 5; step++)
+	{
+		simTotalArtificialHeart.runSimulationFixedDurationJS(0.1 * step);
 	}
 	return 0;
 }
