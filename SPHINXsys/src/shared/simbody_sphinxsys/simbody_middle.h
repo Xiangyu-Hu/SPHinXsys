fbabca67514274542bcc660616f392e15401abc0
wen-yang chu <chu@virtonomy.io>
2021-06-27 17:31:53 -0400

    fix simbody middle h

--- SPHINXsys/src/shared/simbody_sphinxsys/simbody_middle.h
+++ SPHINXsys/src/shared/simbody_sphinxsys/simbody_middle.h
@@ -6,12 +6,18 @@
 
 #ifndef SIMBODY_MIDDLE_H
 #define SIMBODY_MIDDLE_H
+
+#ifdef __linux__ 
 #pragma GCC system_header //for GCC/CLANG
+#else
 #pragma warning(push, 0) //for MSVC
+#endif
 
 #include "SimTKcommon.h"
 #include "SimTKmath.h"
 #include "Simbody.h"
 
+#ifndef __linux__ 
 #pragma warning(pop) //for MSVC
+#endif
 #endif //SIMBODY_MIDDLE_H
