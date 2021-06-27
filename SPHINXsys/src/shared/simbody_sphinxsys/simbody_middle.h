39a85501df5495e5da8bbf6ff168bec6b0e795ad
wen-yang chu <chu@virtonomy.io>
2021-06-27 17:40:55 -0400

    change macro

--- SPHINXsys/src/shared/simbody_sphinxsys/simbody_middle.h
+++ SPHINXsys/src/shared/simbody_sphinxsys/simbody_middle.h
@@ -7,7 +7,7 @@
 #ifndef SIMBODY_MIDDLE_H
 #define SIMBODY_MIDDLE_H
 
-#ifdef __linux__ 
+#ifdef __linux__
 #pragma GCC system_header //for GCC/CLANG
 #elif _WIN32
 #pragma warning(push, 0) //for MSVC
@@ -17,7 +17,7 @@
 #include "SimTKmath.h"
 #include "Simbody.h"
 
-#ifdef _WIN32 
+#ifdef _WIN32
 #pragma warning(pop) //for MSVC
 #endif
 #endif //SIMBODY_MIDDLE_H
