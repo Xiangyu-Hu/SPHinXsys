99e2e02f3d790887d415ca978655bad73eaba8b0
BenceVirtonomy <rochlitz@virtonomy.io>
2021-06-28 09:51:30 +0000

    unit test pointers changed

--- cases_high_level_simulation/test_3d_unit/test_high_level_structural_class.cpp
+++ cases_high_level_simulation/test_3d_unit/test_high_level_structural_class.cpp
@@ -26,7 +26,7 @@ public:
 	vector<string> Get_imported_stl_list_(){ return imported_stl_list_; };
 	Real Get_scale_stl_(){ return scale_stl_; };
 	vector<Vec3d> Get_translation_list_(){ return translation_list_; };
-	Real Get_default_resolution_(){ return default_resolution_; };
+	Real Get_system_resolution_(){ return system_resolution_; };
 	vector<Real> Get_resolution_list_(){ return resolution_list_; };
 	vector<LinearElasticSolid> Get_material_model_list_(){ return material_model_list_; };
 	Real Get_physical_viscosity_(){ return physical_viscosity_; };
@@ -36,28 +36,28 @@ public:
 	In_Output Get_in_output_(){ return in_output_; };
 	// other
 	vector<TriangleMeshShape> Get_body_mesh_list_(){ return body_mesh_list_; };
-	vector<SolidBodyForSimulation*> Get_solid_body_list_(){ return solid_body_list_; };
+	vector<shared_ptr<SolidBodyForSimulation>> Get_solid_body_list_(){ return solid_body_list_; };
 	vector<array<int, 2>> Get_contacting_bodies_list_(){ return contacting_bodies_list_; };
-	vector<SolidContactBodyRelation*> Get_contact_list_(){ return contact_list_; };
-	vector<solid_dynamics::ContactDensitySummation*> Get_contact_density_list_(){ return contact_density_list_; };
-	vector<solid_dynamics::ContactForce*> Get_contact_force_list_(){ return contact_force_list_; };
+	vector<shared_ptr<SolidContactBodyRelation>> Get_contact_list_(){ return contact_list_; };
+	vector<shared_ptr<solid_dynamics::ContactDensitySummation>> Get_contact_density_list_(){ return contact_density_list_; };
+	vector<shared_ptr<solid_dynamics::ContactForce>> Get_contact_force_list_(){ return contact_force_list_; };
 	// for InitializeATimeStep
-	vector<InitializeATimeStep*> Get_initialize_gravity_(){ return initialize_gravity_; };
+	vector<shared_ptr<InitializeATimeStep>> Get_initialize_gravity_(){ return initialize_gravity_; };
 	vector<GravityPair> Get_non_zero_gravity_(){ return non_zero_gravity_; };
 	// for AccelerationForBodyPartInBoundingBox
-	vector<solid_dynamics::AccelerationForBodyPartInBoundingBox*> Get_acceleration_bounding_box_(){ return acceleration_bounding_box_; };
+	vector<shared_ptr<solid_dynamics::AccelerationForBodyPartInBoundingBox>> Get_acceleration_bounding_box_(){ return acceleration_bounding_box_; };
 	vector<AccelTuple> Get_acceleration_bounding_box_tuple_(){ return acceleration_bounding_box_tuple_; };
 	// for SpringDamperConstraintParticleWise
-	vector<solid_dynamics::SpringDamperConstraintParticleWise*> Get_spring_damper_constraint_(){ return spring_damper_constraint_; };
+	vector<shared_ptr<solid_dynamics::SpringDamperConstraintParticleWise>> Get_spring_damper_constraint_(){ return spring_damper_constraint_; };
 	vector<SpringDamperTuple> Get_spring_damper_tuple_(){ return spring_damper_tuple_; };
 	// for ConstrainSolidBodyRegion
-	vector<solid_dynamics::ConstrainSolidBodyRegion*> Get_fixed_constraint_(){ return fixed_constraint_; };
+	vector<shared_ptr<solid_dynamics::ConstrainSolidBodyRegion>> Get_fixed_constraint_(){ return fixed_constraint_; };
 	vector<int> Get_body_indeces_fixed_constraint_(){ return body_indeces_fixed_constraint_; };
 	// for PositionSolidBody
-	vector<solid_dynamics::PositionSolidBody*> Get_position_solid_body_(){ return position_solid_body_; };
+	vector<shared_ptr<solid_dynamics::PositionSolidBody>> Get_position_solid_body_(){ return position_solid_body_; };
 	vector<PositionSolidBodyTuple> Get_position_solid_body_tuple_(){ return position_solid_body_tuple_; };
 	// for PositionScaleSolidBody
-	vector<solid_dynamics::PositionScaleSolidBody*> Get_position_scale_solid_body_(){ return position_scale_solid_body_; };
+	vector<shared_ptr<solid_dynamics::PositionScaleSolidBody>> Get_position_scale_solid_body_(){ return position_scale_solid_body_; };
 	vector<PositionScaleSolidBodyTuple> Get_position_scale_solid_body_tuple_(){ return position_scale_solid_body_tuple_; };
 };
 
@@ -111,7 +111,7 @@ TEST(StructuralSimulation, PositionSolidBodyTuple)
 		EXPECT_EQ(sim.Get_translation_list_()[i], translation_list[i] * scale_stl);
 		EXPECT_EQ(sim.Get_resolution_list_()[i], resolution_list[i] * scale_stl);
 	}
-	EXPECT_EQ(sim.Get_default_resolution_(), resolution_mass * scale_stl);
+	EXPECT_EQ(sim.Get_system_resolution_(), resolution_mass * scale_stl);
 	//=================================================================================================//
 	// test CreateBodyMeshList();
 	EXPECT_EQ(sim.Get_body_mesh_list_().size(), number_of_bodies);
