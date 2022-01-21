a07c34ecf6d6aa732e326d870d65f7c9764868a8
Massoud Rezavand <rezavand@virtonomy.io>
2022-01-21 23:04:24 +0100

    body fitted particle generation

--- tests/2d_examples/test_2d_shell_shell_collision/src/shell_shell_collision.cpp
+++ tests/2d_examples/test_2d_shell_shell_collision/src/shell_shell_collision.cpp
@@ -12,7 +12,7 @@ using namespace SPH;	//Namespace cite here.
 //----------------------------------------------------------------------
 Real DL = 4.0; 					/**< box length. */
 Real DH = 4.0; 					/**< box height. */
-Real resolution_ref = 0.0125; 	/**< reference resolution. */
+Real resolution_ref = 0.025; 	/**< reference resolution. */
 Real BW = resolution_ref * 1.; 	/**< wall width for BCs. */
 BoundingBox system_domain_bounds(Vec2d(-BW, -BW), Vec2d(DL + BW, DH + BW));
 Vec2d ball_center(2.0, 2.0);
@@ -23,9 +23,10 @@ Vec2d initial_velocity = initial_ball_speed*Vec2d(0.0, -1.);
 //----------------------------------------------------------------------
 //	Global paramters on material properties
 //----------------------------------------------------------------------
-Real rho0_s = 1.0e3;
-Real Youngs_modulus = 5.0e4;
-Real poisson = 0.45; 			
+Real rho0_s = 1.0;				   /** Normalized density. */
+Real Youngs_modulus = 1.3024653e6; /** Normalized Youngs Modulus. */
+Real poisson = 0.3;				   /** Poisson ratio. */
+Real physical_viscosity = 200.0;   /** physical damping, here we choose the same value as numerical viscosity. */
 //----------------------------------------------------------------------
 //	Bodies with cases-dependent geometries (ComplexShape).
 //----------------------------------------------------------------------
@@ -55,16 +56,18 @@ public:
 		body_shape_.add<MultiPolygonShape>(multi_polygon);
 	}
 };
-class FreeBall : public ThinStructure
+/** Define application dependent particle generator for thin structure. */
+class BallParticleGenerator : public ParticleGeneratorDirect
 {
 public:
-	FreeBall(SPHSystem& system, std::string body_name) : ThinStructure(system, body_name)
+	BallParticleGenerator() : ParticleGeneratorDirect()
 	{
-		MultiPolygon multi_polygon;
-		multi_polygon.addACircle(ball_center, ball_radius + BW, 100, ShapeBooleanOps::add);
-		multi_polygon.addACircle(ball_center, ball_radius, 100, ShapeBooleanOps::sub);
-		MultiPolygonShape multi_polygon_shape(multi_polygon);
-		body_shape_.add<LevelSetShape>(this, multi_polygon_shape);
+		for (Real t = 0; t <= 4*Pi*ball_radius; t += resolution_ref / 2.) 
+		{
+            Real x = ball_radius * cos(t) + ball_center[0];
+            Real y = ball_radius * sin(t) + ball_center[1];
+			positions_volumes_.push_back(std::make_pair(Vecd(x, y), resolution_ref));
+		}
 	}
 };
 /**
@@ -106,12 +109,10 @@ int main(int ac, char* av[])
 	//----------------------------------------------------------------------
 	//	Creating body, materials and particles.
 	//----------------------------------------------------------------------
-	FreeBall free_ball(sph_system, "FreeBall");
-	SharedPtr<ParticleGenerator> free_ball_particle_generator = makeShared<ParticleGeneratorLattice>();
-	if (!sph_system.run_particle_relaxation_ && sph_system.reload_particles_)
-		free_ball_particle_generator = makeShared<ParticleGeneratorReload>(in_output, free_ball.getBodyName());
-	SharedPtr<LinearElasticSolid> free_ball_material = makeShared<LinearElasticSolid>(rho0_s, Youngs_modulus, poisson);
-	ShellParticles free_ball_particles(free_ball, free_ball_material, free_ball_particle_generator, BW);
+	ThinStructure free_ball(sph_system, "FreeBall", makeShared<SPHAdaptation>(1.15, 1.0));
+	ShellParticles free_ball_particles(free_ball,
+										makeShared<LinearElasticSolid>(rho0_s, Youngs_modulus, poisson),
+										makeShared<BallParticleGenerator>(), BW);
 
 	WallBoundary wall_boundary(sph_system, "Wall");
 	SharedPtr<ParticleGenerator> wall_particle_generator = makeShared<ParticleGeneratorLattice>();
