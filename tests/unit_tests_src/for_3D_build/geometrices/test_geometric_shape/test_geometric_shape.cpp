748bfb3a76719cf0be8a054615c835acdefbe657
xiangyu_hu <xiangyu.hu@tum.de>
2022-05-20 12:06:49 +0200

    small changes

--- tests/unit_tests_src/for_3D_build/geometrices/test_geometric_shape/test_geometric_shape.cpp
+++ tests/unit_tests_src/for_3D_build/geometrices/test_geometric_shape/test_geometric_shape.cpp
@@ -1,14 +1,15 @@
 #include <gtest/gtest.h>
 #include "geometric_shape.h"
+#include "transform_shape.h"
 
 using namespace SPH;
 
 TEST(test_GeometricShapeBox, test_findBounds)
 {
 	Vec3d halfsize(1.0, 0.5, 0.25);
-	SimTK::Transform transfrom(Vec3d(1.0, 0.5, 0.25));
+	SimTK::Transform transform(Vec3d(1.0, 0.5, 0.25));
 
-	GeometricShapeBox brick(halfsize, transfrom);
+	TransformShape<GeometricShapeBox> brick(transform, halfsize);
 	BoundingBox box = brick.findBounds();
 
 	EXPECT_EQ(BoundingBox(Vec3d(0.0, 0.0, 0.0), Vec3d(2.0, 1.0, 0.5)), box);
