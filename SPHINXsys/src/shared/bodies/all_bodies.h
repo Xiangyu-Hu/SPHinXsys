df2daaa381cba629c547627452fc458f116919dc
ChiZhangatTUM <zhangchi0118@gmail.com>
2022-11-08 15:51:12 +0100

    funding number and copy right years.

--- SPHINXsys/src/shared/bodies/all_bodies.h
+++ SPHINXsys/src/shared/bodies/all_bodies.h
@@ -11,9 +11,9 @@
  *																			*
  * SPHinXsys is partially funded by German Research Foundation				*
  * (Deutsche Forschungsgemeinschaft) DFG HU1527/6-1, HU1527/10-1,			*
- *  HU1527/12-1 and Hu1527/12-4												*
+ *  HU1527/12-1 and HU1527/12-4													*
  *                                                                          *
- * Portions copyright (c) 2017-2020 Technical University of Munich and		*
+ * Portions copyright (c) 2017-2022 Technical University of Munich and		*
  * the authors' affiliations.												*
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may  *
