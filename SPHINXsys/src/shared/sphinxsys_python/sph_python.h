076bce7b059d60d8cf19aa28c49544d3286b5bd6
maiyetum95 <108422104+maiyetum95@users.noreply.github.com>
2023-02-27 19:15:28 +0100

    Create sph_python.h

---
+++ SPHINXsys/src/shared/sphinxsys_python/sph_python.h
@@ -0,0 +1,44 @@
+/* -------------------------------------------------------------------------*
+ *								SPHinXsys									*
+ * -------------------------------------------------------------------------*
+ * SPHinXsys (pronunciation: s'finksis) is an acronym from Smoothed Particle*
+ * Hydrodynamics for industrial compleX systems. It provides C++ APIs for	*
+ * physical accurate simulation and aims to model coupled industrial dynamic*
+ * systems including fluid, solid, multi-body dynamics and beyond with SPH	*
+ * (smoothed particle hydrodynamics), a meshless computational method using	*
+ * particle discretization.													*
+ *																			*
+ * SPHinXsys is partially funded by German Research Foundation				*
+ * (Deutsche Forschungsgemeinschaft) DFG HU1527/6-1, HU1527/10-1,			*
+ *  HU1527/12-1 and HU1527/12-4												*
+ *                                                                          *
+ * Portions copyright (c) 2017-2022 Technical University of Munich and		*
+ * the authors' affiliations.												*
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may  *
+ * not use this file except in compliance with the License. You may obtain a*
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.       *
+ *                                                                          *
+ * ------------------------------------------------------------------------*/
+/**
+ * @file sph_python.h
+ * @brief Interface for sph and pyhton code.
+ */
+
+#ifndef SPH_PYTHON_H
+#define SPH_PYTHON_H
+
+namespace SPH {
+
+    class SphPython
+    {
+    public:
+        explicit SphPython() {};
+        virtual ~SphPython() {};
+
+        //*** Functions for override. ***//
+        virtual void runCase(Real End_time) = 0;
+    };
+};
+
+#endif // SPH_PYTHON_H
