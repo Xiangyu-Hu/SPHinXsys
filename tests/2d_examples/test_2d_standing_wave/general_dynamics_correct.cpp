a9ed87b3b02b44976f5e8f91e758fa0ba3faabee
ChiZhangatTUM <zhangchi0118@gmail.com>
2023-03-11 12:28:27 +0100

    remove ununsed varibale.

--- tests/2d_examples/test_2d_standing_wave/general_dynamics_correct.cpp
+++ tests/2d_examples/test_2d_standing_wave/general_dynamics_correct.cpp
@@ -18,8 +18,6 @@ namespace SPH
 		const Neighborhood& inner_neighborhood = inner_configuration_[index_i];
 		for (size_t n = 0; n != inner_neighborhood.current_size_; ++n)
 		{
-			size_t index_j = inner_neighborhood.j_[n];
-
 			Vecd gradW_ij = inner_neighborhood.dW_ijV_j_[n] * inner_neighborhood.e_ij_[n];
 			Vecd r_ji = inner_neighborhood.r_ij_[n] * inner_neighborhood.e_ij_[n];
 			local_configuration -= r_ji * gradW_ij.transpose();
@@ -34,12 +32,9 @@ namespace SPH
 		Matd local_configuration = Eps * Matd::Identity();
 		for (size_t k = 0; k < contact_configuration_.size(); ++k)
 		{
-			StdLargeVec<Real>& Vol_k = *(contact_Vol_[k]);
-			StdLargeVec<Real>& contact_mass_k = *(contact_mass_[k]);
 			Neighborhood& contact_neighborhood = (*contact_configuration_[k])[index_i];
 			for (size_t n = 0; n != contact_neighborhood.current_size_; ++n)
 			{
-				size_t index_j = contact_neighborhood.j_[n];
 				Vecd gradW_ij = contact_neighborhood.dW_ijV_j_[n] * contact_neighborhood.e_ij_[n];
 				Vecd r_ji = contact_neighborhood.r_ij_[n] * contact_neighborhood.e_ij_[n];
 				local_configuration -= r_ji * gradW_ij.transpose();
