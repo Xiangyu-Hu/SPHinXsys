0fd4706d369d0ed2daf53a5b058d785be5adb23c
Xangyu Hu <xangyu.hu@tum.de>
2023-03-21 08:34:48 +0000

    bug in calculatePNorm(

--- SPHINXsys/src/shared/regression_test/dynamic_time_warping_method.hpp
+++ SPHINXsys/src/shared/regression_test/dynamic_time_warping_method.hpp
@@ -44,13 +44,13 @@ namespace SPH
 	template<class ObserveMethodType>
 	Real RegressionTestDynamicTimeWarping<ObserveMethodType>::calculatePNorm(Vecd variable_a, Vecd variable_b)
 	{
-		return (variable_a - variable_b).squaredNorm();
+		return (variable_a - variable_b).norm();
 	};
 	//=================================================================================================//
 	template<class ObserveMethodType>
 	Real RegressionTestDynamicTimeWarping<ObserveMethodType>::calculatePNorm(Matd variable_a, Matd variable_b)
 	{
-		return (variable_a - variable_b).squaredNorm();
+		return (variable_a - variable_b).norm();
 	};
 	//=================================================================================================//
 	template<class ObserveMethodType>
