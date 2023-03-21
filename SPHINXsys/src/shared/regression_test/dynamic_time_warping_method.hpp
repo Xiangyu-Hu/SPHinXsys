2008b81227e0780d4ab176fc0e8adeec58b5ceea
Xangyu Hu <xangyu.hu@tum.de>
2023-03-21 17:21:54 +0000

    revise regression data for twisting column case

--- SPHINXsys/src/shared/regression_test/dynamic_time_warping_method.hpp
+++ SPHINXsys/src/shared/regression_test/dynamic_time_warping_method.hpp
@@ -36,24 +36,6 @@ namespace SPH
 {
 	//=================================================================================================//
 	template<class ObserveMethodType>
-	Real RegressionTestDynamicTimeWarping<ObserveMethodType>::calculatePNorm(Real variable_a, Real variable_b)
-	{
-		return std::abs(variable_a - variable_b);
-	};
-	//=================================================================================================//
-	template<class ObserveMethodType>
-	Real RegressionTestDynamicTimeWarping<ObserveMethodType>::calculatePNorm(Vecd variable_a, Vecd variable_b)
-	{
-		return (variable_a - variable_b).norm();
-	};
-	//=================================================================================================//
-	template<class ObserveMethodType>
-	Real RegressionTestDynamicTimeWarping<ObserveMethodType>::calculatePNorm(Matd variable_a, Matd variable_b)
-	{
-		return (variable_a - variable_b).norm();
-	};
-	//=================================================================================================//
-	template<class ObserveMethodType>
 	StdVec<Real> RegressionTestDynamicTimeWarping<ObserveMethodType>::calculateDTWDistance
 	(DoubleVec<VariableType> dataset_a_, DoubleVec<VariableType> dataset_b_)
 	{
