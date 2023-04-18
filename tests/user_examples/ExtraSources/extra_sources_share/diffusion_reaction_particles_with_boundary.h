2afd1f72aed4480bcefc1d5c238732ab7c7ab17e
Chenxi Zhao <chenxi.zhao@tum.de>
2023-04-18 13:01:20 +0200

    PhiIntermediate has been registered

--- tests/user_examples/ExtraSources/extra_sources_share/diffusion_reaction_particles_with_boundary.h
+++ tests/user_examples/ExtraSources/extra_sources_share/diffusion_reaction_particles_with_boundary.h
@@ -11,7 +11,7 @@
 #include "base_body.h"
 #include "base_material.h"
 #include "diffusion_reaction.h"
-//#include "diffusion_reaction_particles.h"
+#include "diffusion_reaction_particles.h"
 
 namespace SPH
 {
@@ -21,47 +21,17 @@ namespace SPH
 	 * @brief A group of particles with diffusion or/and reactions particle data.
 	 */
 	template <class BaseParticlesType, class DiffusionReactionMaterialType>
-	class DiffusionReactionParticlesWithBoundary : public BaseParticlesType
+	class DiffusionReactionParticlesWithBoundary : public DiffusionReactionParticles<BaseParticlesType, DiffusionReactionMaterialType>
 	{
-	protected:
-		StdVec<std::string> all_species_names_;
-		std::map<std::string, size_t> species_indexes_map_;
-		StdVec<StdLargeVec<Real>*> diffusion_species_;
-		StdVec<StdLargeVec<Real>*> gradient_species_;
-		StdVec<StdLargeVec<Real>*> reactive_species_;
-
 	public:
-		StdVec<StdLargeVec<Real>> all_species_; /**< array of diffusion/reaction scalars */
-		DiffusionReactionMaterialType& diffusion_reaction_material_;
-		static constexpr int NumReactiveSpecies = DiffusionReactionMaterialType::NumReactiveSpecies;
-		typedef DiffusionReactionMaterialType DiffusionReactionMaterial;
-
 		StdLargeVec<Vecd> normal_vector_; /**< normal vector for Neumann or Robin boundary conditions */
 		StdLargeVec<Real> heat_flux_; /**< heat flux for Neumann boundary condition */
 		StdLargeVec<Real> convection_; /**< convection for Robin boundary condition */
 		StdLargeVec<Real> T_infinity_; /**< T_infinity for Robin boundary condition */
 
 		DiffusionReactionParticlesWithBoundary(SPHBody& sph_body, DiffusionReactionMaterialType* diffusion_reaction_material)
-			: BaseParticlesType(sph_body, diffusion_reaction_material),
-			all_species_names_(diffusion_reaction_material->AllSpeciesNames()),
-			species_indexes_map_(diffusion_reaction_material->AllSpeciesIndexMap()),
-			diffusion_reaction_material_(*diffusion_reaction_material)
+			: DiffusionReactionParticles<BaseParticlesType, DiffusionReactionMaterialType>(sph_body, diffusion_reaction_material)
 		{
-			all_species_.resize(all_species_names_.size());
-			const IndexVector& diffusion_species_indexes = diffusion_reaction_material_.DiffusionSpeciesIndexes();
-			const IndexVector& gradient_species_indexes = diffusion_reaction_material_.GradientSpeciesIndexes();
-			for (size_t i = 0; i != diffusion_species_indexes.size(); ++i)
-			{
-				diffusion_species_.push_back(&all_species_[diffusion_species_indexes[i]]);
-				gradient_species_.push_back(&all_species_[gradient_species_indexes[i]]);
-			}
-
-			const IndexVector& reactive_species_indexes = diffusion_reaction_material_.ReactiveSpeciesIndexes();
-			for (size_t i = 0; i != reactive_species_indexes.size(); ++i)
-			{
-				reactive_species_.push_back(&all_species_[reactive_species_indexes[i]]);
-			}
-
 			normal_vector_.resize(this->all_species_.size());
 			heat_flux_.resize(this->all_species_.size());
 			convection_.resize(this->all_species_.size());
@@ -69,26 +39,9 @@ namespace SPH
 		};
 		virtual ~DiffusionReactionParticlesWithBoundary() {};
 
-		StdVec<std::string>& AllSpeciesNames() { return all_species_names_; };
-		std::map<std::string, size_t> AllSpeciesIndexMap() { return species_indexes_map_; };
-		StdVec<StdLargeVec<Real>*>& DiffusionSpecies() { return diffusion_species_; };
-		StdVec<StdLargeVec<Real>*>& GradientSpecies() { return gradient_species_; };
-		StdVec<StdLargeVec<Real>*>& ReactiveSpecies() { return reactive_species_; };
-
 		virtual void initializeOtherVariables() override
 		{
-			BaseParticlesType::initializeOtherVariables();
-
-			std::map<std::string, size_t>::iterator itr;
-			for (itr = species_indexes_map_.begin(); itr != species_indexes_map_.end(); ++itr)
-			{
-				// Register a specie.
-				this->registerVariable(all_species_[itr->second], itr->first);
-				/** the scalars will be sorted if particle sorting is called, Note that we call a template function from a template class. */
-				this->template registerSortableVariable<Real>(itr->first);
-				/** add species to basic output particle data. */
-				this->template addVariableToWrite<Real>(itr->first);
-			}
+			DiffusionReactionParticles<BaseParticlesType, DiffusionReactionMaterialType>::initializeOtherVariables();
 
 			this->registerVariable(normal_vector_, "UnitNormalVector");
 			this->template addVariableToWrite<Vecd>("UnitNormalVector");
@@ -101,7 +54,6 @@ namespace SPH
 
 			this->registerVariable(T_infinity_, "T_infinity");
 			this->template addVariableToWrite<Real>("T_infinity");
-
 		};
 
 		virtual DiffusionReactionParticlesWithBoundary<BaseParticlesType, DiffusionReactionMaterialType>* ThisObjectPtr() override { return this; };
