617e2959543a58556bfc02b6ad7e3f4106c8b9f8
Alberto Guarnieri <nR3D@pm.me>
2023-05-10 12:17:18 +0200

    Added context classes for SYCL execution

---
+++ SPHINXsys/src/shared/particle_dynamics/execution_unit/execution_context.hpp
@@ -0,0 +1,27 @@
+#ifndef SPHINXSYS_EXECUTION_CONTEXT_HPP
+#define SPHINXSYS_EXECUTION_CONTEXT_HPP
+
+#include <sycl/sycl.hpp>
+
+namespace SPH {
+    namespace execution {
+        class ParallelSYCLDevicePolicy;
+
+        template<typename ExecutionUnit>
+        struct Context {
+            template<class ...Args>
+            explicit Context(Args& ...args) {};
+
+            template<class ...Args>
+            explicit Context(Args&& ...args) {};
+        };
+
+        template<>
+        struct Context<ParallelSYCLDevicePolicy> {
+            explicit Context(sycl::handler &cgh) : cgh(cgh) {}
+            sycl::handler& cgh;
+        };
+    }
+}
+
+#endif //SPHINXSYS_EXECUTION_CONTEXT_HPP
