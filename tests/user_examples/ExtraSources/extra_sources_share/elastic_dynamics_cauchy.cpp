1ed0a5f0b8ed722abb75a1540cbba8ddb1d7273d
xiaojing-ubantu <ubantu-xiaojing@tum.de>
2023-05-11 18:45:09 +0200

    run the ctest and reduce the F.detminant() to J, also F transpose

--- tests/user_examples/ExtraSources/extra_sources_share/elastic_dynamics_cauchy.cpp
+++ tests/user_examples/ExtraSources/extra_sources_share/elastic_dynamics_cauchy.cpp
@@ -16,11 +16,13 @@ namespace SPH
 		{
 			pos_[index_i] += vel_[index_i] * dt * 0.5;
 			F_[index_i] += dF_dt_[index_i] * dt * 0.5;
-			rho_[index_i] = rho0_ / F_[index_i].determinant();
-			Matd inverse_F_T = F_[index_i].inverse().transpose();
-			Matd almansi_strain = 0.5 * (Matd::Identity() - (F_[index_i] * F_[index_i].transpose()).inverse());
+			Real J = F_[index_i].determinant();
+			Matd F_T = F_[index_i].transpose();
+			rho_[index_i] = rho0_ / J;
+			Matd inverse_F_T = F_T.inverse();
+			Matd almansi_strain = 0.5 * (Matd::Identity() - (F_[index_i] * F_T).inverse());
 			//obtain the first Piola-Kirchhoff stress from the  Cauchy stress 
-			stress_PK1_B_[index_i]  = F_[index_i].determinant() * elastic_solid_.StressCauchy(almansi_strain, F_[index_i], index_i) * 
+			stress_PK1_B_[index_i]  = J * elastic_solid_.StressCauchy(almansi_strain, F_[index_i], index_i) * 
 									  inverse_F_T * B_[index_i];
 
 		} 
