9df708d31100902ecbbdb797a237318a04ba8d8a
yuanjiajy <jiayuanjy11@gmail.com>
2023-05-18 16:07:57 +0200

    add aortic_valve.cpp

--- src/shared/common/vector_functions.cpp
+++ src/shared/common/vector_functions.cpp
@@ -3,6 +3,21 @@
 namespace SPH
 {
 	//=================================================================================================//
+     Vec3d OcctToEigen(const gp_Pnt &occt_vector)
+   {
+    return Vec3d(occt_vector.X(), occt_vector.Y(), occt_vector.Z());
+   }
+   //=================================================================================================//
+     gp_Pnt EigenToOcct(const Vec3d &eigen_vector)
+    {
+    return gp_Pnt(eigen_vector[0], eigen_vector[1], eigen_vector[2]);
+    }
+   //=================================================================================================//
+   Vec3d OcctVecToEigen(const gp_Vec &occt_vector)
+   {
+    return Vec3d(occt_vector.X(), occt_vector.Y(), occt_vector.Z());
+   }
+	//=================================================================================================//
 	SimTK::Vec2 EigenToSimTK(const Vec2d &eigen_vector)
 	{
 		return SimTK::Vec2(eigen_vector[0], eigen_vector[1]);
