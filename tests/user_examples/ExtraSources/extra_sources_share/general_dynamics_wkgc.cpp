795cfcf68813bbc82fb02cc56823515ede358ccc
Xiangyu Hu <xiangyu.hu@tum.de>
2023-05-21 12:16:37 +0200

    still use complex dynamics

--- tests/user_examples/ExtraSources/extra_sources_share/general_dynamics_wkgc.cpp
+++ tests/user_examples/ExtraSources/extra_sources_share/general_dynamics_wkgc.cpp
@@ -1,7 +1,15 @@
 #include "general_dynamics_wkgc.h"
+#include "base_particles.hpp"
 
 namespace SPH
 {
+//=================================================================================================//
+CorrectionMatrixInner::
+    CorrectionMatrixInner(BaseInnerRelation &inner_relation, int beta, Real alpha)
+    : LocalDynamics(inner_relation.getSPHBody()),
+      GeneralDataDelegateInner(inner_relation),
+      beta_(beta), alpha_(alpha),
+      B_(*particles_->registerSharedVariable<Matd>("WeightedCorrectionMatrix")) {}
 //=================================================================================================//
 void CorrectionMatrixInner::interaction(size_t index_i, Real dt)
 {
@@ -25,10 +33,23 @@ void CorrectionMatrixInner::update(size_t index_i, Real dt)
     B_[index_i] = (det_sqr * inverse + alpha_ * Matd::Identity()) / (alpha_ + det_sqr);
 }
 //=================================================================================================//
-void CorrectionMatrixContact::interaction(size_t index_i, Real dt)
+CorrectionMatrixComplex::
+    CorrectionMatrixComplex(ComplexRelation &complex_relation, int beta, Real alpha)
+    : CorrectionMatrixInner(complex_relation.getInnerRelation(), beta, alpha),
+      GeneralDataDelegateContact(complex_relation.getContactRelation())
 {
-    Matd local_configuration = ZeroData<Matd>::value;
+    for (size_t k = 0; k != contact_particles_.size(); ++k)
+    {
+        contact_mass_.push_back(&(contact_particles_[k]->mass_));
+        contact_Vol_.push_back(&(contact_particles_[k]->Vol_));
+    }
+}
+//=================================================================================================//
+void CorrectionMatrixComplex::interaction(size_t index_i, Real dt)
+{
+    CorrectionMatrixInner::interaction(index_i, dt);
 
+    Matd local_configuration = ZeroData<Matd>::value;
     for (size_t k = 0; k < contact_configuration_.size(); ++k)
     {
         Neighborhood &contact_neighborhood = (*contact_configuration_[k])[index_i];
