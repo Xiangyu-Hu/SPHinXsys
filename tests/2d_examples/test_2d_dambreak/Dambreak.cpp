cc3e33aa63822491cbbcf5871b7fd6cb66555f92
Alberto Guarnieri <nR3D@pm.me>
2023-05-25 19:09:35 +0200

    Use SYCL on supported classes of 2d_dambreak

--- tests/2d_examples/test_2d_dambreak/Dambreak.cpp
+++ tests/2d_examples/test_2d_dambreak/Dambreak.cpp
@@ -90,8 +90,8 @@ int main(int ac, char *av[])
 	InteractionWithUpdate<fluid_dynamics::DensitySummationFreeSurfaceComplex> fluid_density_by_summation(water_block_complex);
 	SimpleDynamics<NormalDirectionFromBodyShape> wall_boundary_normal_direction(wall_boundary);
 	SharedPtr<Gravity> gravity_ptr = makeShared<Gravity>(Vecd(0.0, -gravity_g));
-	SimpleDynamics<TimeStepInitialization> fluid_step_initialization(water_block, gravity_ptr);
-	ReduceDynamics<fluid_dynamics::AdvectionTimeStepSize> fluid_advection_time_step(water_block, U_max);
+	SimpleDynamics<TimeStepInitialization, ParallelSYCLDevicePolicy, DeviceDispatcher> fluid_step_initialization(water_block, gravity_ptr);
+	ReduceDynamics<fluid_dynamics::AdvectionTimeStepSize, ParallelSYCLDevicePolicy, DeviceDispatcher> fluid_advection_time_step(water_block, U_max);
 	ReduceDynamics<fluid_dynamics::AcousticTimeStepSize> fluid_acoustic_time_step(water_block);
 	//----------------------------------------------------------------------
 	//	Define the methods for I/O operations, observations
