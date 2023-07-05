efc5f51b4ccb5c352e958c348a870cee58d966c2
Alberto Guarnieri <nR3D@pm.me>
2023-07-05 12:53:38 +0200

    Added device executable helper class, inherited by classes that can be executed on device

---
+++ src/shared/particle_dynamics/execution_unit/device_executable.hpp
@@ -0,0 +1,22 @@
+#ifndef SPHINXSYS_DEVICE_EXECUTABLE_HPP
+#define SPHINXSYS_DEVICE_EXECUTABLE_HPP
+
+#include "execution_proxy.hpp"
+
+namespace SPH::execution {
+    template<class Base, class Kernel>
+    class DeviceExecutable {
+    public:
+        template<class ...Args>
+        DeviceExecutable(Args&& ...args) : device_proxy(args...) {}
+
+        auto &getDeviceProxy() {
+            return device_proxy;
+        }
+
+    private:
+        ExecutionProxy<Base, Kernel> device_proxy;
+    };
+}
+
+#endif //SPHINXSYS_DEVICE_EXECUTABLE_HPP
