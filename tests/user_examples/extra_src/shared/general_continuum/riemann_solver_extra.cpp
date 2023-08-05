cfdc472f24af828cc15ba96b4ecbebc02eee65ff
RussellSyne <1477856817@qq.com>
2023-08-05 18:04:08 +0200

    fix another bug on Linux-float

--- tests/user_examples/extra_src/shared/general_continuum/riemann_solver_extra.cpp
+++ tests/user_examples/extra_src/shared/general_continuum/riemann_solver_extra.cpp
@@ -4,7 +4,7 @@ namespace SPH
 {
 	Vecd AcousticRiemannSolverExtra::DissipativePJumpExtra(const Vecd& u_jump, const Vecd& e_ij)
 	{
-		return rho0c0_geo_ave_ * u_jump * SMIN(3.0 * SMAX(u_jump.dot(e_ij) * inv_c_ave_, Real(0)), Real(1));
+		return rho0c0_geo_ave_ * u_jump * SMIN(Real(3) * SMAX(u_jump.dot(e_ij) * inv_c_ave_, Real(0)), Real(1));
 	}
 	Vecd DissipativeRiemannSolverExtra::DissipativePJumpExtra(const Vecd& u_jump, const Vecd& e_ij)
 	{
