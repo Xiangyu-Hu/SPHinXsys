4a69538cc4345eec88a04b8b3080f70565d1b6e8
Xiangyu Hu <xiangyu.hu@tum.de>
2024-01-10 22:06:21 +0100

    to test

--- src/for_3D_build/particles/beam_particles.h
+++ src/for_3D_build/particles/beam_particles.h
@@ -75,8 +75,6 @@ class BarParticles : public ShellParticles
     //----------------------------------------------------------------------
     /** get particle volume. */
     virtual Real ParticleVolume(size_t index_i) override { return Vol_[index_i] * thickness_[index_i] * width_[index_i]; }
-    /** get particle mass. */
-    virtual Real ParticleMass(size_t index_i) override { return mass_[index_i] * width_[index_i]; }
     /** Initialize variable for shell particles. */
     virtual void initializeOtherVariables() override;
     virtual BarParticles *ThisObjectPtr() override { return this; };
