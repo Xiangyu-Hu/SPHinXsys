cb8fc30c31b56ac4f59cf9a9b5c13ef7f365c0da
shuoguozhang <shuoguo.zhang@tum.de>
2024-02-08 13:50:44 +0100

    simplify the equation

--- tests/extra_source_and_tests/extra_src/shared/pressure_boundary/pressure_boundary.cpp
+++ tests/extra_source_and_tests/extra_src/shared/pressure_boundary/pressure_boundary.cpp
@@ -13,11 +13,11 @@ FlowPressureBuffer::FlowPressureBuffer(BodyPartByCell &body_part, Vecd normal_ve
 void FlowPressureBuffer::update(size_t index_i, Real dt)
 {
     vel_[index_i] += 2.0*kernel_sum_[index_i] *  getTargetPressure(dt) / rho_[index_i] * dt;
-    Real projectionLength = 0.0;
+
     for (size_t i = 0; i < static_cast<size_t>(vel_[index_i].size()); ++i)
-        projectionLength += vel_[index_i][i] * direction_[i];
-    for (size_t i = 0; i < static_cast<size_t>(vel_[index_i].size()); ++i)
-        vel_[index_i][i] = projectionLength * direction_[i];
+    {
+        vel_[index_i][i] = vel_[index_i].dot(direction_) * direction_[i];
+    }
 }
 } // namespace fluid_dynamics
   //=================================================================================================//
