71d4c3eebeb2514635561843fb891a3523f14884
Xiangyu Hu <xiangyu.hu@tum.de>
2024-04-18 12:07:39 +0200

    this should work for body and body parts

--- src/shared/particle_dynamics/general_dynamics/general_reduce.h
+++ src/shared/particle_dynamics/general_dynamics/general_reduce.h
@@ -92,7 +92,7 @@ class UpperFrontInAxisDirection : public BaseLocalDynamicsReduce<ReduceMax, Dyna
 
   public:
     explicit UpperFrontInAxisDirection(DynamicsIdentifier &identifier, const std::string &name, int axis = lastAxis)
-        : BaseLocalDynamicsReduce<ReduceMax, BodyPartByCell>(identifier),
+        : BaseLocalDynamicsReduce<ReduceMax, DynamicsIdentifier>(identifier),
           GeneralDataDelegateSimple(identifier.getSPHBody()), axis_(axis), pos_(particles_->pos_)
     {
         this->quantity_name_ = name;
