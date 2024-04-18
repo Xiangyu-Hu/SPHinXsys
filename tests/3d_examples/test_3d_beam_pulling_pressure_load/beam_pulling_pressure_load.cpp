8cce11e63de406b25f13a52a5845c4d6a0df4e8d
Xiangyu-Hu <genre.hu@gmail.com>
2024-04-18 21:02:24 +0200

    delete unused object

--- tests/3d_examples/test_3d_beam_pulling_pressure_load/beam_pulling_pressure_load.cpp
+++ tests/3d_examples/test_3d_beam_pulling_pressure_load/beam_pulling_pressure_load.cpp
@@ -139,7 +139,6 @@ int main(int ac, char *av[])
 
     /** Time step size calculation. */
     ReduceDynamics<solid_dynamics::AcousticTimeStepSize> computing_time_step_size(beam_body);
-    SimpleDynamics<solid_dynamics::UpdateElasticNormalDirection> update_beam_normal(beam_body);
 
     /** specify end-time for defining the force-time profile */
     Real end_time = 1;
