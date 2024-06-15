7ae3f2b274f47e6e893b33f5fd7231845b72972b
shuoguozhang <shuoguo.zhang@tum.de>
2024-06-15 15:54:30 +0200

    change test time

--- tests/extra_source_and_tests/test_2d_modified_T_flow/modified_T_flow.cpp
+++ tests/extra_source_and_tests/test_2d_modified_T_flow/modified_T_flow.cpp
@@ -259,7 +259,7 @@ int main(int ac, char *av[])
     size_t number_of_iterations = sph_system.RestartStep();
     int screen_output_interval = 100;
     int observation_sample_interval = screen_output_interval * 2;
-    Real end_time = 30.0;   /**< End time. */
+    Real end_time = 100.0;   /**< End time. */
     Real Output_Time = 0.1; /**< Time stamps for output of body states. */
     Real dt = 0.0;          /**< Default acoustic time step sizes. */
     //----------------------------------------------------------------------
