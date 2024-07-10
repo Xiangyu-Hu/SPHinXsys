3807306bc8b4339a2274a7e0a89976eed4b526db
Feng Wang <feng.wang.aer@tum.de>
2024-07-10 23:07:52 +0200

    to 11_51

--- tests/extra_source_and_tests/2d_turbulent_channel_VI_offset_model/2d_turbulent_channel_VI_offset_model.h
+++ tests/extra_source_and_tests/2d_turbulent_channel_VI_offset_model/2d_turbulent_channel_VI_offset_model.h
@@ -14,7 +14,7 @@ using namespace SPH;
 //----------------------------------------------------------------------
 Real DH = 2.0;                         /**< Channel height. */
 Real DL = 120.0;                         /**< Channel length. */
-Real num_fluid_cross_section = 20.0;
+Real num_fluid_cross_section = 40.0;
 
 //----------------------------------------------------------------------
 //	Unique parameters for turbulence. 
@@ -169,8 +169,8 @@ struct InflowVelocity
         Real run_time = GlobalStaticVariables::physical_time_;
         Real u_ave = run_time < t_ref_ ? 0.5 * u_ref_ * (1.0 - cos(Pi * run_time / t_ref_)) : u_ref_;
         //target_velocity[0] = 1.5 * u_ave * SMAX(0.0, 1.0 - position[1] * position[1] / halfsize_[1] / halfsize_[1]);
-        //target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
-        target_velocity[0] = u_ave;
+        target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
+        //target_velocity[0] = u_ave;
         if (position[1] > half_channel_height)
         {
             std::cout << "Particles out of domain, wrong inlet velocity." << std::endl;
