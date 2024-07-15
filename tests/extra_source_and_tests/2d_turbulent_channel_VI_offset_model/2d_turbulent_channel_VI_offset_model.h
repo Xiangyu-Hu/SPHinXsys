92a4ebd02fa05c601ea6e7b51beaf093ca438a90
Feng Wang <feng.wang.aer@tum.de>
2024-07-15 16:47:31 +0200

    to 11_62

--- tests/extra_source_and_tests/2d_turbulent_channel_VI_offset_model/2d_turbulent_channel_VI_offset_model.h
+++ tests/extra_source_and_tests/2d_turbulent_channel_VI_offset_model/2d_turbulent_channel_VI_offset_model.h
@@ -13,8 +13,8 @@ using namespace SPH;
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
 Real DH = 2.0;                         /**< Channel height. */
-Real DL = 120.0;                         /**< Channel length. */
-Real num_fluid_cross_section = 40.0;
+Real DL = 100.0;                         /**< Channel length. */
+Real num_fluid_cross_section = 20.0;
 
 //----------------------------------------------------------------------
 //	Unique parameters for turbulence. 
