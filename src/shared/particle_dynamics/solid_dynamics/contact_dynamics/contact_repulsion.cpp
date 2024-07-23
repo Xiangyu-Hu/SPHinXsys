85761d8847ae415f3b5597f65889ea8c4939f6a1
WeiyiVirtonomy <kong@virtonomy.io>
2024-07-23 13:55:01 +0200

    change ContactStiffness in base material

--- src/shared/particle_dynamics/solid_dynamics/contact_dynamics/contact_repulsion.cpp
+++ src/shared/particle_dynamics/solid_dynamics/contact_dynamics/contact_repulsion.cpp
@@ -12,18 +12,18 @@ RepulsionForce<Contact<Inner<>>>::
       solid_(DynamicCast<Solid>(this, sph_body_.getBaseMaterial())),
       self_repulsion_density_(*particles_->getVariableDataByName<Real>("SelfRepulsionDensity")),
       vel_(*particles_->getVariableDataByName<Vecd>("Velocity")),
-      contact_impedance_(solid_.ReferenceDensity() * sqrt(solid_.ContactStiffness())) {}
+      contact_impedance_(sqrt(solid_.ReferenceDensity() * solid_.ContactStiffness())) {}
 //=================================================================================================//
 void RepulsionForce<Contact<Inner<>>>::interaction(size_t index_i, Real dt)
 {
-    Real p_i = self_repulsion_density_[index_i] * solid_.ReferenceDensity() * solid_.ContactStiffness();
+    Real p_i = self_repulsion_density_[index_i] * solid_.ContactStiffness();
     Vecd force = Vecd::Zero();
     const Neighborhood &inner_neighborhood = inner_configuration_[index_i];
     for (size_t n = 0; n != inner_neighborhood.current_size_; ++n)
     {
         size_t index_j = inner_neighborhood.j_[n];
         const Vecd &e_ij = inner_neighborhood.e_ij_[n];
-        Real p_star = 0.5 * (p_i + self_repulsion_density_[index_j] * solid_.ReferenceDensity() * solid_.ContactStiffness());
+        Real p_star = 0.5 * (p_i + self_repulsion_density_[index_j] * solid_.ContactStiffness());
         Real impedance_p = 0.5 * contact_impedance_ * (vel_[index_i] - vel_[index_j]).dot(-e_ij);
         // force to mimic pressure
         force -= 2.0 * (p_star + impedance_p) * e_ij * inner_neighborhood.dW_ij_[n] * Vol_[index_j];
@@ -37,7 +37,7 @@ RepulsionForce<Contact<>>::RepulsionForce(BaseContactRelation &solid_body_contac
       solid_(DynamicCast<Solid>(this, sph_body_.getBaseMaterial())),
       repulsion_density_(*particles_->getVariableDataByName<Real>("RepulsionDensity"))
 {
-    const Real contact_stiffness_1 = solid_.ReferenceDensity() * solid_.ContactStiffness();
+    const Real contact_stiffness_1 = solid_.ContactStiffness();
 
     for (size_t k = 0; k != contact_particles_.size(); ++k)
     {
@@ -45,7 +45,7 @@ RepulsionForce<Contact<>>::RepulsionForce(BaseContactRelation &solid_body_contac
         contact_Vol_.push_back(contact_particles_[k]->getVariableDataByName<Real>("VolumetricMeasure"));
         contact_contact_density_.push_back(contact_particles_[k]->getVariableDataByName<Real>("RepulsionDensity"));
 
-        const Real contact_stiffness_k = contact_solids_[k]->ReferenceDensity() * contact_solids_[k]->ContactStiffness();
+        const Real contact_stiffness_k = contact_solids_[k]->ContactStiffness();
         contact_stiffness_ave_.push_back(2 * contact_stiffness_1 * contact_stiffness_k / (contact_stiffness_1 + contact_stiffness_k));
     }
 }
@@ -90,7 +90,7 @@ RepulsionForce<Contact<Wall>>::RepulsionForce(BaseContactRelation &solid_body_co
 //=================================================================================================//
 void RepulsionForce<Contact<Wall>>::interaction(size_t index_i, Real dt)
 {
-    Real p_i = repulsion_density_[index_i] * solid_.ReferenceDensity() * solid_.ContactStiffness();
+    Real p_i = repulsion_density_[index_i] * solid_.ContactStiffness();
     /** Contact interaction. */
     Vecd force = Vecd::Zero();
     for (size_t k = 0; k < contact_configuration_.size(); ++k)
@@ -136,7 +136,7 @@ void RepulsionForce<Wall, Contact<>>::interaction(size_t index_i, Real dt)
             size_t index_j = contact_neighborhood.j_[n];
             Vecd e_ij = contact_neighborhood.e_ij_[n];
 
-            Real p_star = contact_density_k[index_j] * solid_k->ReferenceDensity() * solid_k->ContactStiffness();
+            Real p_star = contact_density_k[index_j] * solid_k->ContactStiffness();
             // force due to pressure
             force -= 2.0 * p_star * e_ij * contact_neighborhood.dW_ij_[n] * Vol_k[index_j];
         }
