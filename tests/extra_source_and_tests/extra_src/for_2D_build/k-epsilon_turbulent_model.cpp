e708930371993efa76fc884c53447956b34c2ac4
Feng Wang <feng.wang.aer@tum.de>
2024-07-26 10:28:33 +0200

    11_78

--- tests/extra_source_and_tests/extra_src/for_2D_build/k-epsilon_turbulent_model.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/k-epsilon_turbulent_model.cpp
@@ -108,7 +108,7 @@ namespace fluid_dynamics
 				size_t index_j = inner_neighborhood.j_[n];
 				Vecd nablaW_ijV_j = inner_neighborhood.dW_ij_[n] * this->Vol_[index_j] * inner_neighborhood.e_ij_[n];
 				
-				Real r_ij = inner_neighborhood.r_ij_[n];
+				//Real r_ij = inner_neighborhood.r_ij_[n];
 				const Vecd& e_ij = inner_neighborhood.e_ij_[n];
 				
 				if (is_near_wall_P2_[index_i] == 10 && is_near_wall_P1_[index_j] == 1)
@@ -118,7 +118,8 @@ namespace fluid_dynamics
                     //velocity_gradient_[index_i] += -2.0 * (vel_i - vel_ps) * nablaW_ijV_j.transpose();
 					//std::cout<<"Calculate velocity gradient according to interpolation!"<<std::endl;
 					//std::cin.get();
-					Vecd vel_diff = velocity_gradient_[index_j] * r_ij * e_ij;
+					//Vecd vel_diff = velocity_gradient_[index_j] * r_ij * e_ij;
+					Vecd vel_diff = velocity_gradient_[index_j] * e_ij;
 					velocity_gradient_[index_i] += - vel_diff * nablaW_ijV_j.transpose();
 				}
 				else
