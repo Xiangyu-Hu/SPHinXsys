071861cc710acdc2966f7decef0eaf10b3c36166
Xiangyu Hu <xiangyu.hu@tum.de>
2024-08-01 22:45:01 +0200

    bug on macro

--- src/shared/common/base_data_type.h
+++ src/shared/common/base_data_type.h
@@ -40,10 +40,10 @@
 #include <typeinfo>
 #include <vector>
 
-#ifndef SPHINXSYS_USE_SYCL
+#if SPHINXSYS_USE_SYCL // Eigen matrix and vector types degraded
 #include <CL/sycl.hpp>
-#define SYCL_DEVICE_ONLY // Eigen matrix and vector types degraded
-#endif                   // SPHINXSYS_USE_SYCL
+#define SYCL_DEVICE_ONLY
+#endif // SPHINXSYS_USE_SYCL
 
 #include <Eigen/Cholesky>
 #include <Eigen/Core>
