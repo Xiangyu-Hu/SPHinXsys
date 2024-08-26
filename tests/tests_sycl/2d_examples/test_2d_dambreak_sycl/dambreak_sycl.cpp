4ff638137fb70f204f167fbbbe0a95f567a4f851
Xiangyu Hu <xiangyu.hu@tum.de>
2024-08-26 20:35:23 +0000

    test locally

--- tests/tests_sycl/2d_examples/test_2d_dambreak_sycl/dambreak_sycl.cpp
+++ tests/tests_sycl/2d_examples/test_2d_dambreak_sycl/dambreak_sycl.cpp
@@ -87,7 +87,7 @@ int main(int ac, char *av[])
     ContactRelation water_wall_contact(water_block, {&wall_boundary});
     ContactRelation fluid_observer_contact(fluid_observer, {&water_block});
 
-    UpdateRelation<BodyRelationUpdate<Inner<>>, execution::ParallelPolicy> water_block_update_inner_relation(water_block_inner);
+    UpdateRelation<BodyRelationUpdate<Inner<>>, execution::ParallelDevicePolicy> water_block_update_inner_relation(water_block_inner);
     DiscreteVariable<UnsignedInt> *dv_particle_offset_water_inner = water_block_inner.getParticleOffset();
 
     //----------------------------------------------------------------------
