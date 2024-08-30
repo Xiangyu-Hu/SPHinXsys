c94395644340b94713e8d8a7ff527c3f02c41d0c
Xiangyu Hu <xiangyu.hu@tum.de>
2024-08-30 18:29:19 +0000

    to find the bug

--- src/shared/shared_ck/neighborhood/neighborhood_ck.hpp
+++ src/shared/shared_ck/neighborhood/neighborhood_ck.hpp
@@ -18,13 +18,14 @@ Neighbor<>::Neighbor(const ExecutionPolicy &ex_policy,
                      SPHAdaptation *sph_adaptation, SPHAdaptation *contact_adaptation,
                      DiscreteVariable<Vecd> *dv_pos, DiscreteVariable<Vecd> *dv_contact_pos)
     : source_pos_(dv_pos->DelegatedDataField(ex_policy)),
-      target_pos_(dv_contact_pos->DelegatedDataField(ex_policy))
+      target_pos_(dv_contact_pos->DelegatedDataField(ex_policy)),
+      kernel_(*sph_adaptation->getKernel())
 {
-    Kernel *kernel = sph_adaptation->getKernel();
-    Kernel *contact_kernel = contact_adaptation->getKernel();
-    kernel_ = kernel->SmoothingLength() > contact_kernel->SmoothingLength()
-                  ? KernelWendlandC2CK(*kernel)
-                  : KernelWendlandC2CK(*contact_kernel);
+    KernelWendlandC2CK contact_kernel(*contact_adaptation->getKernel());
+    if (kernel_.CutOffRadius() < contact_kernel.CutOffRadius())
+    {
+        kernel_ = contact_kernel;
+    }
 }
 //=================================================================================================//
 template <class ExecutionPolicy>
