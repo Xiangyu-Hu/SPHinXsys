a6c9ec60b03bb4c6c73b8aff8ca2402ac2439dcb
Xiangyu Hu <xiangyu.hu@tum.de>
2024-09-03 10:38:15 +0200

    spelling

--- src/shared/particle_dynamics/general_dynamics/kernel_correction.cpp
+++ src/shared/particle_dynamics/general_dynamics/kernel_correction.cpp
@@ -21,7 +21,7 @@ void LinearGradientCorrectionMatrix<Inner<>>::update(size_t index_i, Real dt)
 {
     Real det_sqr = SMAX(alpha_ - B_[index_i].determinant(), Real(0));
     Matd B_T = B_[index_i].transpose(); //for Tikhonov regularization
-    Matd inverse = (B_T * B_[index_i] + sqrEps * Matd::Identity()).inverse() * B_T;
+    Matd inverse = (B_T * B_[index_i] + SqrtEps * Matd::Identity()).inverse() * B_T;
     Real weight1_ = B_[index_i].determinant() / (B_[index_i].determinant() + det_sqr);
     Real weight2_ = det_sqr / (B_[index_i].determinant() + det_sqr);
     B_[index_i] = weight1_ * inverse + weight2_ * Matd::Identity();
