f25a305cfad35e42c080e1753e299a5ccd7f075a
Feng Wang <feng.wang.aer@tum.de>
2024-09-21 18:22:48 +0200

    6

--- tests/extra_source_and_tests/2d_turbulent_HCD_channel/2d_turbulent_HCD_channel.h
+++ tests/extra_source_and_tests/2d_turbulent_HCD_channel/2d_turbulent_HCD_channel.h
@@ -45,8 +45,8 @@ Real characteristic_length = DH; /**<It needs characteristic Length to calculate
 int type_turbulent_inlet = 0;
 Real relaxation_rate_turbulent_inlet = 0.8;
 //** Tag for AMRD *
-int is_AMRD = 0;
-bool is_constrain_normal_velocity_in_P_region = false;
+int is_AMRD = 1;
+bool is_constrain_normal_velocity_in_P_region = true;
 //** Weight for correcting the velocity  gradient in the sub near wall region  *
 Real weight_vel_grad_sub_nearwall = 0.1;
 bool is_always_lattice_arrange_fluid = false;
