0331f167d61908de5473d429823664728e38dc2b
Xiangyu Hu <xiangyu.hu@tum.de>
2024-12-17 08:50:41 +0000

    Revert "warning corrected"
    This reverts commit a8eda3514c025d0b46a4b8ebdda7a5e67aa710e4.

--- src/shared/bodies/base_body_part.cpp
+++ src/shared/bodies/base_body_part.cpp
@@ -8,8 +8,8 @@ BodyPart::BodyPart(SPHBody &sph_body, const std::string &body_part_name)
     : sph_body_(sph_body), part_id_(sph_body.getNewBodyPartID()),
       body_part_name_(body_part_name),
       base_particles_(sph_body.getBaseParticles()),
-      dv_index_list_(nullptr), sv_range_size_(nullptr),
-      pos_(base_particles_.getVariableDataByName<Vecd>("Position")) {}
+      pos_(base_particles_.getVariableDataByName<Vecd>("Position")),
+      dv_index_list_(nullptr), sv_range_size_(nullptr) {}
 //=================================================================================================//
 BodyPartByParticle::BodyPartByParticle(SPHBody &sph_body, const std::string &body_part_name)
     : BodyPart(sph_body, body_part_name),
