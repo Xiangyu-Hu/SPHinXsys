7b4af8d7d6aed6d08b8faea658841745571970f4
Feng Wang <feng.wang.aer@tum.de>
2025-01-29 10:46:12 +0100

    try deal with sycl

--- tests/extra_source_and_tests/extra_src/for_2D_build/k-epsilon_turbulent_model.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/k-epsilon_turbulent_model.cpp
@@ -378,7 +378,7 @@ void TurbuViscousForce<Inner<>>::interaction(size_t index_i, Real dt)
 
         Real u_jump = (vel_[index_i] - vel_[index_j]).dot(e_ij);
 
-        Real dissipation = rho_[index_i] * smoothing_length_ * SMIN(3.0 * SMAX(u_jump, Real(0)), c0_);
+        Real dissipation = rho_[index_i] * smoothing_length_ * SMIN(Real(3.0) * SMAX(u_jump, Real(0)), c0_);
         Real dissipation_judge = dissipation;
 
         Vecd shear_stress_eij_corrected = shear_stress_eij;
