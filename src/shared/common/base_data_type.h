b2f03c93563dc7fc70159f857d961d7d1309088f
Shuang Li <913274387@qq.com>
2025-02-20 10:20:17 +0800

    Update

--- src/shared/common/base_data_type.h
+++ src/shared/common/base_data_type.h
@@ -56,10 +56,11 @@ namespace SPH
 #if SPHINXSYS_USE_FLOAT
 using Real = float;
 using UnsignedInt = u_int32_t;
-using sycl::sin;
+namespace math = sycl;
 #else
 using Real = double;
 using UnsignedInt = size_t;
+namespace math = std;
 #endif // SPHINXSYS_USE_FLOAT
 
 /** Vector with integers. */
