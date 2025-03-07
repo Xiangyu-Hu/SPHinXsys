6fa12a6e744eed6281d4f39d88804eb6145cd797
Xiangyu Hu <xiangyu.hu@tum.de>
2025-03-07 21:46:19 +0000

    Revert "Revert "naming and format""
    This reverts commit 0056b1258d02d79c070cc0b062477d1d43167f94.

--- src/shared/regression_test/dynamic_time_warping_method.hpp
+++ src/shared/regression_test/dynamic_time_warping_method.hpp
@@ -14,15 +14,16 @@ namespace SPH
 {
 //=================================================================================================//
 template <class ObserveMethodType>
-StdVec<Real> RegressionTestDynamicTimeWarping<ObserveMethodType>::calculateDTWDistance(BiVector<VariableType> dataset_a_, BiVector<VariableType> dataset_b_)
+StdVec<Real> RegressionTestDynamicTimeWarping<ObserveMethodType>::
+    calculateDTWDistance(BiVector<VariableType> dataset_a_, BiVector<VariableType> dataset_b_)
 {
     /* define the container to hold the dtw distance.*/
     StdVec<Real> dtw_distance;
-    for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int k = 0; k != this->observation_; ++k)
     {
         int window_size_ = 5;
-        int a_length = dataset_a_[observation_index].size();
-        int b_length = dataset_b_[observation_index].size();
+        int a_length = dataset_a_[k].size();
+        int b_length = dataset_b_[k].size();
 
         if (b_length > 1.1 * a_length || b_length < 0.9 * a_length)
         {
@@ -32,20 +33,23 @@ StdVec<Real> RegressionTestDynamicTimeWarping<ObserveMethodType>::calculateDTWDi
         }
 
         /** create a 2D vector with [a_length, b_length] to contain the local DTW distance value. */
-        BiVector<Real> local_dtw_distance(a_length, StdVec<Real>(b_length, 0));
-        local_dtw_distance[0][0] = calculatePNorm(dataset_a_[observation_index][0], dataset_b_[observation_index][0]);
-        for (int index_i = 1; index_i < a_length; ++index_i)
-            local_dtw_distance[index_i][0] = local_dtw_distance[index_i - 1][0] + calculatePNorm(dataset_a_[observation_index][index_i], dataset_b_[observation_index][0]);
-        for (int index_j = 1; index_j < b_length; ++index_j)
-            local_dtw_distance[0][index_j] = local_dtw_distance[0][index_j - 1] + calculatePNorm(dataset_a_[observation_index][0], dataset_b_[observation_index][index_j]);
+        BiVector<Real> local_distance(a_length, StdVec<Real>(b_length, 0));
+        local_distance[0][0] = calculatePNorm(dataset_a_[k][0], dataset_b_[k][0]);
+        for (int i = 1; i < a_length; ++i)
+            local_distance[i][0] = local_distance[i - 1][0] +
+                                       calculatePNorm(dataset_a_[k][i], dataset_b_[k][0]);
+        for (int j = 1; j < b_length; ++j)
+            local_distance[0][j] = local_distance[0][j - 1] +
+                                       calculatePNorm(dataset_a_[k][0], dataset_b_[k][j]);
 
         /** add locality constraint */
         window_size_ = SMAX(window_size_, ABS(a_length - b_length));
-        for (int index_i = 1; index_i != a_length; ++index_i)
-            for (int index_j = SMAX(1, index_i - window_size_); index_j != SMIN(b_length, index_i + window_size_); ++index_j)
-                local_dtw_distance[index_i][index_j] = calculatePNorm(dataset_a_[observation_index][index_i], dataset_b_[observation_index][index_j]) +
-                                                       SMIN(local_dtw_distance[index_i - 1][index_j], local_dtw_distance[index_i][index_j - 1], local_dtw_distance[index_i - 1][index_j - 1]);
-        dtw_distance.push_back(local_dtw_distance[a_length - 1][b_length - 1]);
+        for (int i = 1; i != a_length; ++i)
+            for (int j = SMAX(1, i - window_size_); j != SMIN(b_length, i + window_size_); ++j)
+                local_distance[i][j] =
+                    calculatePNorm(dataset_a_[k][i], dataset_b_[k][j]) +
+                    SMIN(local_distance[i - 1][j], local_distance[i][j - 1], local_distance[i - 1][j - 1]);
+        dtw_distance.push_back(local_distance[a_length - 1][b_length - 1]);
     }
     return dtw_distance;
 };
@@ -71,13 +75,13 @@ void RegressionTestDynamicTimeWarping<ObserveMethodType>::readDTWDistanceFromXml
     if (this->number_of_run_ > 1)
     {
         dtw_distance_xml_engine_in_.loadXmlFile(dtw_distance_filefullpath_);
-        SimTK::Xml::Element element_name_dtw_distance_ = dtw_distance_xml_engine_in_.root_element_;
-        SimTK::Xml::element_iterator ele_ite = element_name_dtw_distance_.element_begin();
-        for (; ele_ite != element_name_dtw_distance_.element_end(); ++ele_ite)
-            for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+        SimTK::Xml::Element element_name_dtw_distance = dtw_distance_xml_engine_in_.root_element_;
+        SimTK::Xml::element_iterator ele_ite = element_name_dtw_distance.element_begin();
+        for (; ele_ite != element_name_dtw_distance.element_end(); ++ele_ite)
+            for (int k = 0; k != this->observation_; ++k)
             {
-                std::string attribute_name_ = this->quantity_name_ + "_" + std::to_string(observation_index);
-                dtw_distance_xml_engine_in_.getRequiredAttributeValue(ele_ite, attribute_name_, dtw_distance_[observation_index]);
+                std::string attribute_name = this->quantity_name_ + "_" + std::to_string(k);
+                dtw_distance_xml_engine_in_.getRequiredAttributeValue(ele_ite, attribute_name, dtw_distance_[k]);
             }
     }
 };
@@ -89,9 +93,9 @@ void RegressionTestDynamicTimeWarping<ObserveMethodType>::updateDTWDistance()
     {
         StdVec<Real> dtw_distance_local_(this->observation_, 0);
         dtw_distance_local_ = calculateDTWDistance(this->current_result_trans_, this->result_in_);
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+        for (int k = 0; k != this->observation_; ++k)
         {
-            dtw_distance_new_[observation_index] = SMAX(dtw_distance_local_[observation_index], dtw_distance_[observation_index], dtw_distance_new_[observation_index]);
+            dtw_distance_new_[k] = SMAX(dtw_distance_local_[k], dtw_distance_[k], dtw_distance_new_[k]);
         }
         this->result_in_.clear();
     }
@@ -103,10 +107,10 @@ void RegressionTestDynamicTimeWarping<ObserveMethodType>::writeDTWDistanceToXml(
     SimTK::Xml::Element DTWElement = dtw_distance_xml_engine_out_.root_element_;
     dtw_distance_xml_engine_out_.addChildToElement(DTWElement, "DTWDistance");
     SimTK::Xml::element_iterator ele_ite = DTWElement.element_begin();
-    for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int k = 0; k != this->observation_; ++k)
     {
-        std::string attribute_name_ = this->quantity_name_ + "_" + std::to_string(observation_index);
-        dtw_distance_xml_engine_out_.setAttributeToElement(ele_ite, attribute_name_, dtw_distance_new_[observation_index]);
+        std::string attribute_name = this->quantity_name_ + "_" + std::to_string(k);
+        dtw_distance_xml_engine_out_.setAttributeToElement(ele_ite, attribute_name, dtw_distance_new_[k]);
     }
     dtw_distance_xml_engine_out_.writeToXmlFile(dtw_distance_filefullpath_);
 };
@@ -117,13 +121,14 @@ bool RegressionTestDynamicTimeWarping<ObserveMethodType>::compareDTWDistance(Rea
     if (this->number_of_run_ > 1)
     {
         int count_not_converged_ = 0;
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+        for (int k = 0; k != this->observation_; ++k)
         {
-            if (std::abs(dtw_distance_[observation_index] - dtw_distance_new_[observation_index]) > threshold_value)
+            if (std::abs(dtw_distance_[k] - dtw_distance_new_[k]) > threshold_value)
             {
                 count_not_converged_++;
-                std::cout << "The DTW distance of " << this->quantity_name_ << " [" << observation_index << "] is not converged." << std::endl;
-                std::cout << "The old DTW distance is " << dtw_distance_[observation_index] << ", and the new DTW distance is " << dtw_distance_new_[observation_index] << "." << std::endl;
+                std::cout << "The DTW distance of " << this->quantity_name_ << " [" << k << "] is not converged." << std::endl;
+                std::cout << "The old DTW distance is " << dtw_distance_[k] << ", and the new DTW distance is "
+                          << dtw_distance_new_[k] << "." << std::endl;
             }
         };
 
@@ -132,14 +137,16 @@ bool RegressionTestDynamicTimeWarping<ObserveMethodType>::compareDTWDistance(Rea
             if (this->label_for_repeat_ == 4)
             {
                 this->converged_ = "true";
-                std::cout << "The DTW distance of " << this->quantity_name_ << " are converged enough times, and rum will stop now." << std::endl;
+                std::cout << "The DTW distance of " << this->quantity_name_
+                          << " are converged enough times, and run will stop now." << std::endl;
                 return true;
             }
             else
             {
                 this->converged_ = "false";
                 this->label_for_repeat_++;
-                std::cout << "The DTW distance of " << this->quantity_name_ << " are converged, and this is the " << this->label_for_repeat_
+                std::cout << "The DTW distance of " << this->quantity_name_
+                          << " are converged, and this is the " << this->label_for_repeat_
                           << " times. They should be converged more times to be stable." << std::endl;
             }
         }
@@ -159,22 +166,24 @@ void RegressionTestDynamicTimeWarping<ObserveMethodType>::resultTest()
     int test_wrong = 0;
     StdVec<Real> dtw_distance_current_;
     dtw_distance_current_ = calculateDTWDistance(this->result_in_, this->current_result_trans_);
-    for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int k = 0; k != this->observation_; ++k)
     {
-        if (dtw_distance_current_[observation_index] > 1.01 * dtw_distance_[observation_index])
+        if (dtw_distance_current_[k] > 1.01 * dtw_distance_[k])
         {
-            std::cout << "The maximum distance of " << this->quantity_name_ << "[" << observation_index << "] is " << dtw_distance_[observation_index]
-                      << ", and the current distance is " << dtw_distance_current_[observation_index] << "." << std::endl;
+            std::cout << "The maximum distance of " << this->quantity_name_ << "[" << k << "] is " << dtw_distance_[k]
+                      << ", and the current distance is " << dtw_distance_current_[k] << "." << std::endl;
             test_wrong++;
         }
     };
     if (test_wrong == 0)
     {
-        std::cout << "The DTW distance of " << this->quantity_name_ << " between current result and this previous local result is within exception!" << std::endl;
+        std::cout << "The DTW distance of " << this->quantity_name_
+                  << " between current result and this previous local result is within exception!" << std::endl;
     }
     else
     {
-        std::cout << "The DTW distance of " << this->quantity_name_ << " between current result and this previous local result is beyond exception!" << std::endl;
+        std::cout << "The DTW distance of " << this->quantity_name_
+                  << " between current result and this previous local result is beyond exception!" << std::endl;
         std::cout << "Please try again. If it still post this sentence, the result is not correct!" << std::endl;
         exit(1);
     }
--- src/shared/regression_test/ensemble_average_method.hpp
+++ src/shared/regression_test/ensemble_average_method.hpp
@@ -15,20 +15,20 @@ namespace SPH
 template <class ObserveMethodType>
 void RegressionTestEnsembleAverage<ObserveMethodType>::calculateNewVariance(TriVector<VariableType> &result)
 {
-    for (int snapshot_index = 0; snapshot_index != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++snapshot_index)
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int l = 0; l != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++l)
+        for (int k = 0; k != this->observation_; ++k)
             for (int run_index = 0; run_index != this->number_of_run_; ++run_index)
             {
-                variance_new_[snapshot_index][observation_index] = transform_component(
-                    variance_new_[snapshot_index][observation_index],
+                variance_new_[l][k] = transform_component(
+                    variance_new_[l][k],
                     [&](Real variance_new, Real variance, Real result, Real meanvalue_new)
                     { return SMAX(
                           variance_new,
                           variance,
                           (Real)pow(result - meanvalue_new, 2),
                           (Real)pow(meanvalue_new * Real(0.01), 2)); },
-                    variance_[snapshot_index][observation_index], result[run_index][snapshot_index][observation_index],
-                    meanvalue_new_[snapshot_index][observation_index]);
+                    variance_[l][k], result[run_index][l][k],
+                    meanvalue_new_[l][k]);
             }
 }
 //=================================================================================================//
@@ -37,21 +37,21 @@ int RegressionTestEnsembleAverage<ObserveMethodType>::compareParameter(
     std::string par_name, BiVector<VariableType> &parameter, BiVector<VariableType> &parameter_new, VariableType &threshold)
 {
     int count = 0;
-    for (int snapshot_index = 0; snapshot_index != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++snapshot_index)
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int l = 0; l != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++l)
+        for (int k = 0; k != this->observation_; ++k)
         {
             for_each_component(
-                parameter[snapshot_index][observation_index], [&](Real para, Real para_new, Real thr)
+                parameter[l][k], [&](Real para, Real para_new, Real thr)
                 {
                     Real relative_value = ABS(para - para_new) / (para + TinyReal);
                     if (relative_value > thr)
                     {
-                        std::cout << par_name << ": " << this->quantity_name_ << "[" << observation_index << "]"
-                                  << this->element_tag_[snapshot_index] << " is not converged, and difference is "
+                        std::cout << par_name << ": " << this->quantity_name_ << "[" << k << "]"
+                                  << this->element_tag_[l] << " is not converged, and difference is "
                                   << relative_value << std::endl;
                         count++;
                     } },
-                parameter_new[snapshot_index][observation_index], threshold);
+                parameter_new[l][k], threshold);
         }
     return count;
 }
@@ -63,22 +63,22 @@ int RegressionTestEnsembleAverage<ObserveMethodType>::testNewResult(
 {
     int count = 0;
     std::cout << "The current length difference is " << diff << "." << std::endl;
-    for (int snapshot_index = 0; snapshot_index != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++snapshot_index)
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+    for (int l = 0; l != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++l)
+        for (int k = 0; k != this->observation_; ++k)
         {
             for_each_component(
-                current_result[snapshot_index][observation_index], [&](Real cur_result, Real mean, Real var)
+                current_result[l][k], [&](Real cur_result, Real mean, Real var)
                 {
                     Real relative_value = (pow(cur_result - mean, 2) - var) / (var + TinyReal);
                     if (relative_value > 0.01)
                     {
-                        std::cout << this->quantity_name_ << "[" << observation_index << "]" 
-                        << this->element_tag_[snapshot_index] << " is beyond the exception, and difference is " 
+                        std::cout << this->quantity_name_ << "[" << k << "]" 
+                        << this->element_tag_[l] << " is beyond the exception, and difference is " 
                         << relative_value << std::endl;
                         count++;
                     } },
-                meanvalue[snapshot_index + diff][observation_index],
-                variance[snapshot_index + diff][observation_index]);
+                meanvalue[l + diff][k],
+                variance[l + diff][k]);
         }
     return count;
 }
@@ -116,8 +116,8 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::setupAndCorrection()
         else
         {
             this->mean_variance_xml_engine_in_.loadXmlFile(this->mean_variance_filefullpath_);
-            SimTK::Xml::Element mean_element_ = this->mean_variance_xml_engine_in_.getChildElement("Mean_Element");
-            this->number_of_snapshot_old_ = std::distance(mean_element_.element_begin(), mean_element_.element_end());
+            SimTK::Xml::Element mean_element = this->mean_variance_xml_engine_in_.getChildElement("Mean_Element");
+            this->number_of_snapshot_old_ = std::distance(mean_element.element_begin(), mean_element.element_end());
 
             BiVector<VariableType> temp(SMAX(this->snapshot_, this->number_of_snapshot_old_), StdVec<VariableType>(this->observation_));
             meanvalue_ = temp;
@@ -127,7 +127,7 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::setupAndCorrection()
             if (this->number_of_snapshot_old_ < this->snapshot_)
             {
                 this->snapshot_difference_ = this->snapshot_ - this->number_of_snapshot_old_;
-                for (int delete_ = 0; delete_ != this->snapshot_difference_; ++delete_)
+                for (int k = 0; k != this->snapshot_difference_; ++k)
                     this->current_result_.pop_back();
             }
             else if (this->number_of_snapshot_old_ > this->snapshot_)
@@ -151,14 +151,14 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::readMeanVarianceFromXml()
 {
     if (this->number_of_run_ > 1)
     {
-        SimTK::Xml::Element mean_element_ = this->mean_variance_xml_engine_in_.getChildElement("Mean_Element");
-        SimTK::Xml::Element variance_element_ = this->mean_variance_xml_engine_in_.getChildElement("Variance_Element");
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
+        SimTK::Xml::Element mean_element = this->mean_variance_xml_engine_in_.getChildElement("Mean_Element");
+        SimTK::Xml::Element variance_element = this->mean_variance_xml_engine_in_.getChildElement("Variance_Element");
+        for (int k = 0; k != this->observation_; ++k)
         {
-            this->xmlmemory_io_.readDataFromXmlMemory(this->mean_variance_xml_engine_in_,
-                                                      mean_element_, observation_index, this->meanvalue_, this->quantity_name_);
-            this->xmlmemory_io_.readDataFromXmlMemory(this->mean_variance_xml_engine_in_,
-                                                      variance_element_, observation_index, this->variance_, this->quantity_name_);
+            this->readDataFromXmlMemory(this->mean_variance_xml_engine_in_,
+                                                      mean_element, k, this->meanvalue_, this->quantity_name_);
+            this->readDataFromXmlMemory(this->mean_variance_xml_engine_in_,
+                                                      variance_element, k, this->variance_, this->quantity_name_);
         }
     }
 }
@@ -169,7 +169,7 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::updateMeanVariance()
     /** Unify the length of result and meanvalue. */
     if (this->number_of_run_ > 1)
     {
-        for (int delete_ = 0; delete_ != this->snapshot_difference_; ++delete_)
+        for (int k = 0; k != this->snapshot_difference_; ++k)
         {
             meanvalue_.pop_back();
             variance_.pop_back();
@@ -179,11 +179,11 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::updateMeanVariance()
     variance_new_ = variance_;
 
     /** update the meanvalue of the result. */
-    for (int snapshot_index = 0; snapshot_index != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++snapshot_index)
-        for (int observation_index = 0; observation_index != this->observation_; ++observation_index)
-            meanvalue_new_[snapshot_index][observation_index] = (meanvalue_[snapshot_index][observation_index] * (this->number_of_run_ - 1) +
-                                                                 this->current_result_[snapshot_index][observation_index]) /
-                                                                this->number_of_run_;
+    for (int l = 0; l != SMIN(this->snapshot_, this->number_of_snapshot_old_); ++l)
+        for (int k = 0; k != this->observation_; ++k)
+            meanvalue_new_[l][k] = (meanvalue_[l][k] * (this->number_of_run_ - 1) +
+                                    this->current_result_[l][k]) /
+                                   this->number_of_run_;
     /** Update the variance of the result. */
     calculateNewVariance(this->result_);
 }
@@ -192,13 +192,15 @@ template <class ObserveMethodType>
 void RegressionTestEnsembleAverage<ObserveMethodType>::writeMeanVarianceToXml()
 {
     this->mean_variance_xml_engine_out_.addElementToXmlDoc("Mean_Element");
-    SimTK::Xml::Element mean_element_ = this->mean_variance_xml_engine_out_.getChildElement("Mean_Element");
-    this->xmlmemory_io_.writeDataToXmlMemory(this->mean_variance_xml_engine_out_, mean_element_, this->meanvalue_new_,
-                                             SMIN(this->snapshot_, this->number_of_snapshot_old_), this->observation_, this->quantity_name_, this->element_tag_);
+    SimTK::Xml::Element mean_element = this->mean_variance_xml_engine_out_.getChildElement("Mean_Element");
+    this->writeDataToXmlMemory(
+        this->mean_variance_xml_engine_out_, mean_element, this->meanvalue_new_,
+        SMIN(this->snapshot_, this->number_of_snapshot_old_), this->observation_, this->quantity_name_, this->element_tag_);
     this->mean_variance_xml_engine_out_.addElementToXmlDoc("Variance_Element");
-    SimTK::Xml::Element variance_element_ = this->mean_variance_xml_engine_out_.getChildElement("Variance_Element");
-    this->xmlmemory_io_.writeDataToXmlMemory(this->mean_variance_xml_engine_out_, variance_element_, this->variance_new_,
-                                             SMIN(this->snapshot_, this->number_of_snapshot_old_), this->observation_, this->quantity_name_, this->element_tag_);
+    SimTK::Xml::Element variance_element = this->mean_variance_xml_engine_out_.getChildElement("Variance_Element");
+    this->writeDataToXmlMemory(
+        this->mean_variance_xml_engine_out_, variance_element, this->variance_new_,
+        SMIN(this->snapshot_, this->number_of_snapshot_old_), this->observation_, this->quantity_name_, this->element_tag_);
     this->mean_variance_xml_engine_out_.writeToXmlFile(this->mean_variance_filefullpath_);
 }
 //=================================================================================================//
@@ -218,14 +220,16 @@ bool RegressionTestEnsembleAverage<ObserveMethodType>::compareMeanVariance()
             {
                 this->converged_ = "true";
                 this->label_for_repeat_++;
-                std::cout << "The meanvalue and variance of " << this->quantity_name_ << " are converged enough times, and run will stop now." << std::endl;
+                std::cout << "The meanvalue and variance of " << this->quantity_name_
+                          << " are converged enough times, and run will stop now." << std::endl;
                 return true;
             }
             else
             {
                 this->converged_ = "false";
                 this->label_for_repeat_++;
-                std::cout << "The variance of " << this->quantity_name_ << " are also converged, and this is the " << this->label_for_repeat_
+                std::cout << "The variance of " << this->quantity_name_
+                          << " are also converged, and this is the " << this->label_for_repeat_
                           << " times. They should be converged more times to be stable." << std::endl;
                 return false;
             }
@@ -234,7 +238,8 @@ bool RegressionTestEnsembleAverage<ObserveMethodType>::compareMeanVariance()
         {
             this->converged_ = "false";
             this->label_for_repeat_ = 0;
-            std::cout << "The variance of " << this->quantity_name_ << " are not converged " << count_not_converged_v << " times." << std::endl;
+            std::cout << "The variance of " << this->quantity_name_ << " are not converged "
+                      << count_not_converged_v << " times." << std::endl;
             return false;
         };
     }
@@ -242,7 +247,8 @@ bool RegressionTestEnsembleAverage<ObserveMethodType>::compareMeanVariance()
     {
         this->converged_ = "false";
         this->label_for_repeat_ = 0;
-        std::cout << "The meanvalue of " << this->quantity_name_ << " are not converged " << count_not_converged_m << " times." << std::endl;
+        std::cout << "The meanvalue of " << this->quantity_name_ << " are not converged "
+                  << count_not_converged_m << " times." << std::endl;
         return false;
     }
 }
@@ -257,7 +263,7 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::resultTest()
     else
     {
         /** Unify the length of meanvalue, variance, old result, new result. */
-        for (int delete_ = 0; delete_ != this->snapshot_difference_; ++delete_)
+        for (int k = 0; k != this->snapshot_difference_; ++k)
         {
             meanvalue_.pop_back();
             variance_.pop_back();
@@ -266,7 +272,8 @@ void RegressionTestEnsembleAverage<ObserveMethodType>::resultTest()
     }
     /* draw the conclusion. */
     if (test_wrong == 0)
-        std::cout << "The result of " << this->quantity_name_ << " are correct based on the ensemble averaged regression test!" << std::endl;
+        std::cout << "The result of " << this->quantity_name_
+                  << " are correct based on the ensemble averaged regression test!" << std::endl;
     else
     {
         std::cout << "There are " << test_wrong << " snapshots are not within the expected range." << std::endl;
