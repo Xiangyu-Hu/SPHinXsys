67b9c63e04c35878ade6a346a5490ce43da32081
Shuaihao-Zhang <1477856817@qq.com>
2025-03-18 11:05:11 +0800

    fix data type

--- src/shared/particle_dynamics/fluid_dynamics/fluid_time_step.cpp
+++ src/shared/particle_dynamics/fluid_dynamics/fluid_time_step.cpp
@@ -42,7 +42,7 @@ Real SurfaceTensionTimeStep::reduce(size_t index_i, Real dt)
 {
     Real acceleration_scale = 4.0 * h_min_ *
                               (force_[index_i] + force_prior_[index_i]).norm() / mass_[index_i];
-    return SMAX(fluid_.getSoundSpeed(p_[index_i], rho_[index_i]) + vel_[index_i].norm(), acceleration_scale, sqrt(2*Pi* surface_tension_coeff_/(rho0_*h_min_)));
+    return SMAX(fluid_.getSoundSpeed(p_[index_i], rho_[index_i]) + vel_[index_i].norm(), acceleration_scale, (Real)sqrt(2.0*Pi* surface_tension_coeff_/(rho0_*h_min_)));
 }
 //=================================================================================================//
 AdvectionTimeStep::
