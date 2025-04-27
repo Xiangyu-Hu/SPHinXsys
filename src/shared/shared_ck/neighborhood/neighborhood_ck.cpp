0030294b5eb0572974f20f57274187a6b81cc5af
Xiangyu Hu <xiangyu.hu@tum.de>
2025-04-27 14:49:51 +0000

    bug found

--- src/shared/shared_ck/neighborhood/neighborhood_ck.cpp
+++ src/shared/shared_ck/neighborhood/neighborhood_ck.cpp
@@ -5,6 +5,6 @@ namespace SPH
 //=================================================================================================//
 Neighbor<>::NeighborCriterion::NeighborCriterion(Neighbor<> &neighbor)
     : source_pos_(neighbor.source_pos_), target_pos_(neighbor.target_pos_),
-      kernel_size_square_(neighbor.kernel_size_square_) {}
+      inv_h_(neighbor.inv_h_), kernel_size_square_(neighbor.kernel_size_square_) {}
 //=================================================================================================//
 } // namespace SPH
