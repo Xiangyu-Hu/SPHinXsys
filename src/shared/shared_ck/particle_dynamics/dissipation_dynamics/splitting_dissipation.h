cc81e4223db745efee36bd72fdd9ce3d85d2767e
Xiangyu Hu <xiangyu.hu@tum.de>
2025-05-31 20:57:00 +0000

    to make examples

--- src/shared/shared_ck/particle_dynamics/dissipation_dynamics/splitting_dissipation.h
+++ src/shared/shared_ck/particle_dynamics/dissipation_dynamics/splitting_dissipation.h
@@ -70,7 +70,7 @@ class Dissipation<Base, DissipationType, RelationType<Parameters...>>
     DiscreteVariable<DataType> *dv_variable_;
 };
 
-// Note: ConservativeDamping methods are not for obtaining accurate solution,
+// Note: ConservativeDamping method is not for obtaining accurate solution,
 // but for stabilization the system with a numerical damping coefficient.
 // This method is unconditionally stable and has zero-order consistency.
 // It is used to damp the high-frequency oscillations in the system.
@@ -94,6 +94,64 @@ class ConservativeDamping<Inner<Splitting, DampingType, Parameters...>>
         template <class ExecutionPolicy, class EncloserType>
         InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser);
         void interact(size_t index_i, Real dt = 0.0);
+    };
+};
+
+// Note: ProjectionDissipation method is for obtaining accurate solution for steady problems.
+// This method is unconditionally stable, but has small conservative errors.
+// Therefore, it is not used for transient system which requires conservation properties.
+template <typename...>
+class ProjectionDissipation;
+
+template <typename DissipationType, typename... Parameters>
+class ProjectionDissipation<Inner<Splitting, DissipationType, Parameters...>>
+    : public Dissipation<Base, DissipationType, Inner<Parameters...>>
+{
+    using DataType = typename DissipationType::DataType;
+    using BaseDissipationType = Dissipation<Base, DissipationType, Inner<Parameters...>>;
+
+  public:
+    explicit ProjectionDissipation(Inner<Parameters...> &inner_relation, const std::string &variable_name);
+    virtual ~ProjectionDissipation() {};
+
+    class InteractKernel : public BaseDissipationType::InteractKernel
+    {
+      public:
+        template <class ExecutionPolicy, class EncloserType>
+        InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser);
+        void interact(size_t index_i, Real dt = 0.0);
+    };
+};
+
+// Note: PairwiseDissipation method is for obtaining solution for both for steady and transient problems.
+// This method is unconditionally stable, conservative and able to obtaining converged solution.
+// However the method is less accurate than explicit methods.
+// Therefore, it is used when explicit time-step size is too small to be practical,
+// or when the problem is too stiff for explicit methods.
+template <typename...>
+class PairwiseDissipation;
+
+template <typename DissipationType, typename... Parameters>
+class PairwiseDissipation<Inner<Splitting, DissipationType, Parameters...>>
+    : public Dissipation<Base, DissipationType, Inner<Parameters...>>
+{
+    using DataType = typename DissipationType::DataType;
+    using BaseDissipationType = Dissipation<Base, DissipationType, Inner<Parameters...>>;
+    using InverseVolumetricCapacity = typename DissipationType::InverseVolumetricCapacity;
+
+  public:
+    explicit PairwiseDissipation(Inner<Parameters...> &inner_relation, const std::string &variable_name);
+    virtual ~PairwiseDissipation() {};
+
+    class InteractKernel : public BaseDissipationType::InteractKernel
+    {
+      public:
+        template <class ExecutionPolicy, class EncloserType>
+        InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser);
+        void interact(size_t index_i, Real dt = 0.0);
+
+      protected:
+        InverseVolumetricCapacity capacity_;
     };
 };
 } // namespace SPH
--- src/shared/shared_ck/particle_dynamics/dissipation_dynamics/splitting_dissipation.hpp
+++ src/shared/shared_ck/particle_dynamics/dissipation_dynamics/splitting_dissipation.hpp
@@ -86,5 +86,107 @@ void ConservativeDamping<Inner<Splitting, DampingType, Parameters...>>::Interact
     this->variable_[index_i] -= ttl_out_flux / this->Vol_[index_i];
 }
 //=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+ProjectionDissipation<Inner<Splitting, DissipationType, Parameters...>>::
+    ProjectionDissipation(Inner<Parameters...> &inner_relation, const std::string &variable_name)
+    : BaseDissipationType(inner_relation, variable_name) {}
+//=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+template <class ExecutionPolicy, class EncloserType>
+ProjectionDissipation<Inner<Splitting, DissipationType, Parameters...>>::InteractKernel::
+    InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser)
+    : BaseDissipationType::InteractKernel(ex_policy, encloser) {}
+//=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+void ProjectionDissipation<Inner<Splitting, DissipationType, Parameters...>>::InteractKernel::
+    interact(size_t index_i, Real dt)
+{
+    // compute the error and parameters
+    DataType error = this->zero_error_;
+    Real parameter_a = 0.0;
+    std::array<Real, MaximumNeighborhoodSize> parameter_b;
+    Real parameter_c = 0.0;
+    Real ttl_Vol = this->Vol_[index_i];
+    DataType ttl_variable = this->variable_[index_i] * this->Vol_[index_i];
+    int neighbor_j = 0;
+    for (UnsignedInt n = this->FirstNeighbor(index_i); n != this->LastNeighbor(index_i); ++n)
+    {
+        UnsignedInt index_j = this->neighbor_index_[n];
+
+        parameter_b[neighbor_j] = 2.0 * this->dis_coeff_(index_i, index_j) *
+                                  this->dW_ij(index_i, index_j) * this->Vol_[index_j] * dt /
+                                  this->vec_r_ij(index_i, index_j).norm();
+        error -= (this->variable_[index_i] - this->variable_[index_j]) * parameter_b[neighbor_j];
+
+        parameter_a += parameter_b[neighbor_j];
+        parameter_c += parameter_b[neighbor_j] * parameter_b[neighbor_j];
+        ttl_Vol += this->Vol_[index_j];
+        ttl_variable += this->variable_[index_j] * this->Vol_[index_j];
+        ++neighbor_j;
+    }
+    parameter_a = (parameter_a - 1.0);
+
+    // update the variable
+    DataType parameter_k = error / (parameter_c + parameter_a * parameter_a + TinyReal);
+    this->variable_[index_i] += parameter_k * parameter_a;
+    neighbor_j = 0;
+    for (UnsignedInt n = this->FirstNeighbor(index_i); n != this->LastNeighbor(index_i); ++n)
+    {
+        UnsignedInt index_j = this->neighbor_index_[n];
+        this->variable_[index_j] -= parameter_k * parameter_b[neighbor_j];
+        ++neighbor_j;
+    }
+}
+//=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+PairwiseDissipation<Inner<Splitting, DissipationType, Parameters...>>::
+    PairwiseDissipation(Inner<Parameters...> &inner_relation, const std::string &variable_name)
+    : BaseDissipationType(inner_relation, variable_name) {}
+//=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+template <class ExecutionPolicy, class EncloserType>
+PairwiseDissipation<Inner<Splitting, DissipationType, Parameters...>>::InteractKernel::
+    InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser)
+    : BaseDissipationType::InteractKernel(ex_policy, encloser),
+      capacity_(encloser.dissipation_model_) {}
+//=================================================================================================//
+template <typename DissipationType, typename... Parameters>
+void PairwiseDissipation<Inner<Splitting, DissipationType, Parameters...>>::InteractKernel::
+    interact(size_t index_i, Real dt)
+{
+    std::array<Real, MaximumNeighborhoodSize> parameter_b;
+    int neighbor_j = 0;
+    for (UnsignedInt n = this->FirstNeighbor(index_i); n != this->LastNeighbor(index_i); ++n) // forward sweep
+    {
+        UnsignedInt index_j = this->neighbor_index_[n];
+
+        DataType pair_difference = (this->variable_[index_i] - this->variable_[index_j]);
+        parameter_b[neighbor_j] = this->dis_coeff_(index_i, index_j) * this->dW_ij(index_i, index_j) *
+                                  this->Vol_[index_i] * this->Vol_[index_j] * dt /
+                                  this->vec_r_ij(index_i, index_j).norm();
+
+        DataType increment = parameter_b[neighbor_j] * pair_difference /
+                             (capacity_(index_i) * capacity_(index_j) -
+                              parameter_b[neighbor_j] * (capacity_(index_i) + capacity_(index_j)));
+        this->variable_[index_i] += increment * capacity_(index_j);
+        this->variable_[index_j] -= increment * capacity_(index_i);
+        ++neighbor_j;
+    }
+
+    --neighbor_j;
+    for (UnsignedInt n = this->LastNeighbor(index_i); n != this->FirstNeighbor(index_i); --n) // backward sweep
+    {
+        UnsignedInt index_j = this->neighbor_index_[n - 1];
+
+        DataType pair_difference = (this->variable_[index_i] - this->variable_[index_j]);
+        DataType increment = parameter_b[neighbor_j] * pair_difference /
+                             (capacity_(index_i) * capacity_(index_j) -
+                              parameter_b[neighbor_j] * (capacity_(index_i) + capacity_(index_j)));
+        this->variable_[index_i] += increment * capacity_(index_j);
+        this->variable_[index_j] -= increment * capacity_(index_i);
+        --neighbor_j;
+    }
+}
+//=================================================================================================//
 } // namespace SPH
 #endif // SPLITTING_DISSIPATION_HPP
