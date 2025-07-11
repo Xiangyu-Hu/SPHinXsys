8bccc425ec4f24292758fae8f0481a01f26e234b
Xiangyu Hu <xiangyu.hu@tum.de>
2025-07-11 16:09:28 +0000

    no repeat initialization

--- src/shared/io_system/io_log.cpp
+++ src/shared/io_system/io_log.cpp
@@ -9,16 +9,18 @@ namespace SPH
 namespace Log
 {
 //=================================================================================================//
-std::shared_ptr<spdlog::logger> logger;
+std::shared_ptr<spdlog::logger> logger; // Global logger pointer
 //=================================================================================================//
 void init(IOEnvironment &io_environment)
 {
-    // Create a logger with a file sink
-    logger = spdlog::basic_logger_mt("sphinxsys_logger", io_environment.outputFolder() + "/sphinxsys.log");
-    logger->flush_on(spdlog::level::info);                          // Flush logs on info level
-    spdlog::set_default_logger(logger);                             // Set the default logger to the created logger
-    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] [thread %t] %v"); // Set the log format
-    spdlog::flush_every(std::chrono::seconds(1));                   // Flush logs every second
+    if (!logger) // Create a logger with a file sink
+    {
+        logger = spdlog::basic_logger_mt("sphinxsys_logger", io_environment.outputFolder() + "/sphinxsys.log");
+        logger->flush_on(spdlog::level::info);                          // Flush logs on info level
+        spdlog::set_default_logger(logger);                             // Set the default logger to the created logger
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] [thread %t] %v"); // Set the log format
+        spdlog::flush_every(std::chrono::seconds(1));                   // Flush logs every second
+    }
 }
 //=================================================================================================//
 std::shared_ptr<spdlog::logger> get()
