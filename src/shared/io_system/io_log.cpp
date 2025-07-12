0c9ce51cedbcf8baedbdc5544ecec398265b0d82
xiangyu_hu <xaingyu.hu@tum.de>
2025-07-12 23:47:11 +0200

    log output to bin

--- src/shared/io_system/io_log.cpp
+++ src/shared/io_system/io_log.cpp
@@ -15,7 +15,7 @@ void init(IOEnvironment &io_environment)
 {
     if (!logger) // Create a logger with a file sink
     {
-        logger = spdlog::basic_logger_mt("sphinxsys_logger", io_environment.outputFolder() + "/sphinxsys.log");
+        logger = spdlog::basic_logger_mt("sphinxsys_logger", "sphinxsys.log");
         logger->flush_on(spdlog::level::info);                          // Flush logs on info level
         spdlog::set_default_logger(logger);                             // Set the default logger to the created logger
         spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] [thread %t] %v"); // Set the log format
