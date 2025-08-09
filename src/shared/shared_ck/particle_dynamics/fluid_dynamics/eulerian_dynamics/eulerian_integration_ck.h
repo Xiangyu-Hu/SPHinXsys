8667cfaf6ed51c31567b4504238445ca774e6190
Xiangyu Hu <xiangyu.hu@tum.de>
2025-08-09 14:46:19 +0000

    begin

---
+++ src/shared/shared_ck/particle_dynamics/fluid_dynamics/eulerian_dynamics/eulerian_integration_ck.h
@@ -0,0 +1,52 @@
+/* ------------------------------------------------------------------------- *
+ *                                SPHinXsys                                  *
+ * ------------------------------------------------------------------------- *
+ * SPHinXsys (pronunciation: s'finksis) is an acronym from Smoothed Particle *
+ * Hydrodynamics for industrial compleX systems. It provides C++ APIs for    *
+ * physical accurate simulation and aims to model coupled industrial dynamic *
+ * systems including fluid, solid, multi-body dynamics and beyond with SPH   *
+ * (smoothed particle hydrodynamics), a meshless computational method using  *
+ * particle discretization.                                                  *
+ *                                                                           *
+ * SPHinXsys is partially funded by German Research Foundation               *
+ * (Deutsche Forschungsgemeinschaft) DFG HU1527/6-1, HU1527/10-1,            *
+ *  HU1527/12-1 and HU1527/12-4.                                             *
+ *                                                                           *
+ * Portions copyright (c) 2017-2025 Technical University of Munich and       *
+ * the authors' affiliations.                                                *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may   *
+ * not use this file except in compliance with the License. You may obtain a *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.        *
+ *                                                                           *
+ * ------------------------------------------------------------------------- */
+/**
+ * @file 	eulerian_integration_ck.h
+ * @brief 	Here, we define the common weakly compressible eulerian classes for fluid dynamics.
+ * @author	Zhentong Wang and Xiangyu Hu
+ */
+#ifndef EULERIAN_INTEGRATION_CK_H
+#define EULERIAN_INTEGRATION_CK_H
+
+#include "base_fluid_dynamics.h"
+#include "interaction_ck.hpp"
+
+namespace SPH
+{
+namespace fluid_dynamics
+{
+template <class DataDelegationType>
+class EulerianIntegrationCK : public BaseInteractionType
+{
+  public:
+    template <class BaseRelationType>
+    explicit EulerianIntegrationCK(BaseRelationType &base_relation);
+    virtual ~EulerianIntegrationCK() {};
+
+  protected:
+    DiscreteVariable<Vecd> *dv_mom_, *dv_dmom_dt_;
+    DiscreteVariable<Real> *dv_dmass_dt_, *dv_Vol_;
+};
+} // namespace fluid_dynamics
+} // namespace SPH
+#endif // EULERIAN_INTEGRATION_CK_H
