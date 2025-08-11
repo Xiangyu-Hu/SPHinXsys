62fdae9d74770db317a6c18599926cd0cb728849
Xiangyu Hu <xiangyu.hu@tum.de>
2025-08-11 16:38:59 +0000

    draft hpp file

--- src/shared/shared_ck/particle_dynamics/general_dynamics/kernel_integral.hpp
+++ src/shared/shared_ck/particle_dynamics/general_dynamics/kernel_integral.hpp
@@ -13,22 +13,22 @@ KernelIntegralBase<BaseInteractionType>::KernelIntegralBase(DynamicsIdentifier &
       dv_kernel_integral_(
           this->particles_->template registerStateVariable<Real>("KernelIntegral")) {}
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
-KernelIntegral<Inner<KernelCorrectionType, Parameters...>>::KernelIntegral(
+template <typename... Parameters>
+KernelIntegral<Inner<Parameters...>>::KernelIntegral(
     Inner<Parameters...> &inner_relation)
     : KernelIntegralBase<Interaction<Inner<Parameters...>>>(inner_relation) {}
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
+template <typename... Parameters>
 template <class ExecutionPolicy, class EncloserType>
-KernelIntegral<Inner<KernelCorrectionType, Parameters...>>::InteractKernel::
+KernelIntegral<Inner<Parameters...>>::InteractKernel::
     InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser)
     : BaseInteraction::InteractKernel(ex_policy, encloser),
       W0_(this->W(ZeroData<Vecd>::value)),
       kernel_integral_(encloser.dv_kernel_integral_->DelegatedData(ex_policy)),
       Vol_(encloser.dv_Vol_->DelegatedData(ex_policy)) {}
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
-void KernelIntegral<Inner<KernelCorrectionType, Parameters...>>::
+template <typename... Parameters>
+void KernelIntegral<Inner<Parameters...>>::
     InteractKernel::interact(size_t index_i, Real dt)
 {
     Real integral = W0_ * Vol_[index_i];
@@ -40,21 +40,21 @@ void KernelIntegral<Inner<KernelCorrectionType, Parameters...>>::
     kernel_integral_[index_i] = integral;
 }
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
-KernelIntegral<Contact<KernelCorrectionType, Parameters...>>::
+template <typename... Parameters>
+KernelIntegral<Contact<Parameters...>>::
     KernelIntegral(Contact<Parameters...> &contact_relation)
     : KernelIntegralBase<Interaction<Contact<Parameters...>>>(contact_relation) {}
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
+template <typename... Parameters>
 template <class ExecutionPolicy, class EncloserType>
-KernelIntegral<Contact<KernelCorrectionType, Parameters...>>::InteractKernel::
+KernelIntegral<Contact<Parameters...>>::InteractKernel::
     InteractKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser, UnsignedInt contact_index)
     : BaseInteraction::InteractKernel(ex_policy, encloser, contact_index),
       kernel_integral_(encloser.dv_kernel_integral_->DelegatedData(ex_policy)),
       contact_Vol_(encloser.dv_contact_Vol_[contact_index]->DelegatedData(ex_policy)) {}
 //=================================================================================================//
-template <class KernelCorrectionType, typename... Parameters>
-void KernelIntegral<Contact<KernelCorrectionType, Parameters...>>::
+template <typename... Parameters>
+void KernelIntegral<Contact<Parameters...>>::
     InteractKernel::interact(size_t index_i, Real dt)
 {
     Real integral = 0.0;
