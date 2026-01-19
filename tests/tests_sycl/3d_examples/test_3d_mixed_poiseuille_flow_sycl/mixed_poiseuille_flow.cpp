add0edaf5cf6a11cd05fe64d13b8bd8d75a482de
Xiangyu Hu <xiangyu.hu@tum.de>
2026-01-19 15:58:44 +0000

    particle relaxation stabalize simulation

--- tests/tests_sycl/3d_examples/test_3d_mixed_poiseuille_flow_sycl/mixed_poiseuille_flow.cpp
+++ tests/tests_sycl/3d_examples/test_3d_mixed_poiseuille_flow_sycl/mixed_poiseuille_flow.cpp
@@ -13,7 +13,7 @@ using namespace SPH;
 //----------------------------------------------------------------------
 Real DL = 0.0075;                   /**< Channel length. */
 Real DH = 0.001;                    /**< Channel height. */
-Real global_resolution = DH / 15.0; /**< Reference particle spacing. */
+Real global_resolution = DH / 16.0; /**< Reference particle spacing. */
 Real error_tolerance = 5 * 0.01;    // Less than 3 percent when resolution is DH/20 and DL/DH = 20
 
 Real BW = global_resolution * 4; /**< Extending width for BCs. */
@@ -187,6 +187,7 @@ int main(int ac, char *av[])
     //	Build up an SPHSystem and IO environment.
     //----------------------------------------------------------------------
     SPHSystem sph_system(system_domain_bounds, global_resolution);
+    sph_system.setRunParticleRelaxation(false);
     sph_system.handleCommandlineOptions(ac, av);
     //----------------------------------------------------------------------
     //	Creating bodies with corresponding materials and particles.
@@ -199,20 +200,123 @@ int main(int ac, char *av[])
 
     auto wall_body_shape = makeShared<ComplexShape>("WallBody");
     wall_body_shape->add<TriangleMeshShapeCylinder>(Vec3d(1., 0., 0.), DH * 0.5 + BW,
-                                                    DL * 0.5 + BW, SimTK_resolution,
-                                                    translation_fluid);
+                                                    DL * 0.5, SimTK_resolution,
+                                                    translation_fluid, "OuterBoundary");
     wall_body_shape->subtract<TriangleMeshShapeCylinder>(Vec3d(1., 0., 0.), DH * 0.5,
-                                                         DL * 0.5 + 2.0 * BW, SimTK_resolution,
-                                                         translation_fluid);
+                                                         DL * 0.5 + BW, SimTK_resolution,
+                                                         translation_fluid, "InnerBoundary");
 
     FluidBody water_body(sph_system, water_body_shape);
+    SolidBody wall(sph_system, wall_body_shape);
+    //----------------------------------------------------------------------
+    //	Run particle relaxation for body-fitted distribution if chosen.
+    //----------------------------------------------------------------------
+    if (sph_system.RunParticleRelaxation())
+    {
+        LevelSetShape *outer_level_set_shape = wall.defineComponentLevelSetShape("OuterBoundary", 2.0)
+                                                   ->writeLevelSet();
+        wall.generateParticles<BaseParticles, Lattice>();
+        NearShapeSurface near_wall_outer_surface(wall, "OuterBoundary");
+
+        LevelSetShape *water_level_set_shape = water_body.defineBodyLevelSetShape(2.0)
+                                                   ->writeLevelSet();
+        water_body.generateParticles<BaseParticles, Lattice>();
+        NearShapeSurface near_water_surface(water_body);
+
+        Inner<> wall_inner(wall);
+        Inner<> water_inner(water_body);
+        Contact<> wall_contact(wall, {&water_body});
+        //----------------------------------------------------------------------
+        //	Methods used for particle relaxation.
+        //----------------------------------------------------------------------
+        SPHSolver sph_solver(sph_system);
+        auto &main_methods = sph_solver.addParticleMethodContainer(par_host);
+        auto &host_methods = sph_solver.addParticleMethodContainer(par_host);
+
+        auto &wall_cell_linked_list = main_methods.addCellLinkedListDynamics(wall);
+        auto &water_body_cell_linked_list = main_methods.addCellLinkedListDynamics(water_body);
+        auto &water_update_inner_relation = main_methods.addRelationDynamics(water_inner);
+        auto &wall_update_complex_relation = main_methods.addRelationDynamics(wall_inner, wall_contact);
+
+        auto &random_cylinder_particles = main_methods.addStateDynamics<RandomizeParticlePositionCK>(wall);
+        auto &random_water_body_particles = main_methods.addStateDynamics<RandomizeParticlePositionCK>(water_body);
+
+        auto &water_relaxation_residual =
+            main_methods.addInteractionDynamics<KernelGradientIntegral, NoKernelCorrectionCK>(water_inner)
+                .addPostStateDynamics<LevelsetKernelGradientIntegral>(water_body, *water_level_set_shape);
+        auto &water_relaxation_scaling = main_methods.addReduceDynamics<RelaxationScalingCK>(water_body);
+        auto &water_update_particle_position = main_methods.addStateDynamics<PositionRelaxationCK>(water_body);
+        auto &water_level_set_bounding = main_methods.addStateDynamics<LevelsetBounding>(near_water_surface);
+
+        auto &wall_relaxation_residual =
+            main_methods.addInteractionDynamics<KernelGradientIntegral, NoKernelCorrectionCK>(wall_inner)
+                .addPostContactInteraction<Boundary, NoKernelCorrectionCK>(wall_contact)
+                .addPostStateDynamics<LevelsetKernelGradientIntegral>(wall, *outer_level_set_shape);
+        auto &wall_relaxation_scaling = main_methods.addReduceDynamics<RelaxationScalingCK>(wall);
+        auto &wall_update_particle_position = main_methods.addStateDynamics<PositionRelaxationCK>(wall);
+        auto &wall_level_set_bounding = main_methods.addStateDynamics<LevelsetBounding>(near_wall_outer_surface);
+        //----------------------------------------------------------------------
+        //	Run on CPU after relaxation finished and output results.
+        //----------------------------------------------------------------------
+        auto &wall_normal_direction = host_methods.addStateDynamics<NormalFromSubShapeAndOpCK>(wall, "InnerBoundary");
+        //----------------------------------------------------------------------
+        //	Define simple file input and outputs functions.
+        //----------------------------------------------------------------------
+        auto &body_state_recorder = main_methods.addBodyStateRecorder<BodyStatesRecordingToVtpCK>(sph_system);
+        body_state_recorder.addToWrite<Vecd>(wall, "NormalDirection");
+        auto &write_particle_reload_files = main_methods.addIODynamics<ReloadParticleIOCK>(StdVec<SPHBody *>{&wall, &water_body});
+        write_particle_reload_files.addToReload<Vecd>(wall, "NormalDirection");
+        //----------------------------------------------------------------------
+        //	Prepare the simulation with cell linked list, configuration
+        //	and case specified initial condition if necessary.
+        //----------------------------------------------------------------------
+        random_cylinder_particles.exec();
+        random_water_body_particles.exec();
+        //----------------------------------------------------------------------
+        //	First output before the simulation.
+        //----------------------------------------------------------------------
+        body_state_recorder.writeToFile(0);
+        //----------------------------------------------------------------------
+        //	Particle relaxation time stepping start here.
+        //----------------------------------------------------------------------
+        int ite_p = 0;
+        while (ite_p < 2000)
+        {
+            water_body_cell_linked_list.exec();
+            water_update_inner_relation.exec();
+
+            water_relaxation_residual.exec();
+            Real water_body_relaxation_step = water_relaxation_scaling.exec();
+            water_update_particle_position.exec(water_body_relaxation_step);
+            water_level_set_bounding.exec();
+
+            wall_cell_linked_list.exec();
+            wall_update_complex_relation.exec();
+            wall_relaxation_residual.exec();
+            Real wall_relaxation_step = wall_relaxation_scaling.exec();
+            wall_update_particle_position.exec(wall_relaxation_step);
+            wall_level_set_bounding.exec();
+
+            ite_p += 1;
+            if (ite_p % 100 == 0)
+            {
+                std::cout << std::fixed << std::setprecision(9) << "Relaxation steps N = " << ite_p << "\n";
+                body_state_recorder.writeToFile(ite_p);
+            }
+        }
+        std::cout << "The physics relaxation process finish !" << std::endl;
+        wall_normal_direction.exec();
+        write_particle_reload_files.writeToFile();
+
+        return 0;
+    }
     water_body.defineClosure<WeaklyCompressibleFluid, Viscosity>(ConstructArgs(rho0_f, c_f), mu_f);
     ParticleBuffer<ReserveSizeFactor> particle_buffer(0.5);
-    water_body.generateParticlesWithReserve<BaseParticles, Lattice>(particle_buffer);
+    water_body.generateParticlesWithReserve<BaseParticles, Reload>(particle_buffer, water_body.getName());
 
-    SolidBody wall(sph_system, wall_body_shape);
     wall.defineMaterial<Solid>();
-    wall.generateParticles<BaseParticles, Lattice>();
+    wall.generateParticles<BaseParticles, Reload>(wall.getName())
+        ->reloadExtraVariable<Vecd>("NormalDirection");
     // Add observer
     {
         int num_points = 15;
@@ -272,7 +376,6 @@ int main(int ac, char *av[])
     // Finally, the auxiliary models such as time step estimator, initial condition,
     // boundary condition and other constraints should be defined.
     //----------------------------------------------------------------------
-    StateDynamics<execution::ParallelPolicy, NormalFromBodyShapeCK> wall_normal_direction(wall); // run on CPU
     StateDynamics<MainExecutionPolicy, fluid_dynamics::AdvectionStepSetup> water_advection_step_setup(water_body);
     StateDynamics<MainExecutionPolicy, fluid_dynamics::UpdateParticlePosition> water_update_particle_position(water_body);
     InteractionDynamicsCK<MainExecutionPolicy, LinearCorrectionMatrixComplex>
@@ -306,12 +409,12 @@ int main(int ac, char *av[])
     body_states_recording.addToWrite<Real>(water_body, "Pressure");
     body_states_recording.addToWrite<int>(water_body, "BufferIndicator");
     body_states_recording.addToWrite<int>(water_body, "Indicator");
+    body_states_recording.addToWrite<Vecd>(wall, "NormalDirection");
     ObservedQuantityRecording<MainExecutionPolicy, Vec3d, RestoringCorrection> write_centerline_velocity("Velocity", velocity_observer_contact);
     //----------------------------------------------------------------------
     //	Prepare the simulation with cell linked list, configuration
     //	and case specified initial condition if necessary.
     //----------------------------------------------------------------------
-    wall_normal_direction.exec();
     water_cell_linked_list.exec();
     wall_cell_linked_list.exec();
     water_body_update_complex_relation.exec();
