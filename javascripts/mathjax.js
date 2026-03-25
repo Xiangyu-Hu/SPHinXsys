f682ea2e1d41c8722a6994ca6fe8edd8cdb35200
Xiangyu Hu <xiangyu.hu@tum.de>
2026-03-25 14:42:19 +0000

    Deployed 1c0a7406d4 with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,9 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+  }
+};
