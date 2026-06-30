df7e28021bb8db3866707f02d8ddcc95295c74d1
Xiangyu Hu <xiangyu.hu@tum.de>
2026-06-30 13:07:02 +0000

    split assign mixture fraction from update reference density

--- src/shared/shared_ck/particle_dynamics/fluid_dynamics/fluid_mixture/fluid_mixture_state.h
+++ src/shared/shared_ck/particle_dynamics/fluid_dynamics/fluid_mixture/fluid_mixture_state.h
@@ -37,70 +37,17 @@ namespace SPH
 {
 namespace fluid_dynamics
 {
-class ReferenceDensityUpdate : public LocalDynamics
-{
-  public:
-    explicit ReferenceDensityUpdate(SPHBody &sph_body)
-        : LocalDynamics(sph_body),
-          fluid_mixture(DynamicCast<WeaklyCompressibleMultiSpecies>(
-              this, this->sph_body_->getMatterMaterial())),
-          ca_inv_rho0_list_(fluid_mixture.caInvReferenceDensity()),
-          dv_Y_list_(fluid_mixture.dvMassFraction()),
-          dv_rho0_(fluid_mixture.dvReferenceDensity()),
-          dv_mass_(particles_->getVariableByName<Real>("Mass")) {};
-    virtual ~ReferenceDensityUpdate() {};
-
-    class UpdateKernel
-    {
-      public:
-        template <class ExecutionPolicy>
-        UpdateKernel(const ExecutionPolicy &ex_policy, ReferenceDensityUpdate &encloser)
-            : inv_rho0_list_(encloser.ca_inv_rho0_list_->DelegatedArrayView(ex_policy)),
-              Y_list_(encloser.dv_Y_list_->DelegatedMultiEntryView(ex_policy)),
-              rho0_(encloser.dv_rho0_->DelegatedDataView(ex_policy)),
-              mass_(encloser.dv_mass_->DelegatedDataView(ex_policy)){};
-
-        void update(size_t index_i, Real dt = 0.0)
-        {
-            Real sum = 0.0;
-            Real old_rho0_inv = 1.0 / rho0_[index_i];
-            Real old_mass = mass_[index_i];
-            for (size_t k = 0; k != Y_list_.Width(); ++k)
-            {
-                sum += Y_list_[index_i][k] * inv_rho0_list_[k];
-            }
-            rho0_[index_i] = 1.0 / sum;
-            mass_[index_i] = rho0_[index_i] * old_mass * old_rho0_inv;
-        };
-
-      protected:
-        ArrayView<Real> inv_rho0_list_;
-        MultiEntryView<Real> Y_list_;
-        DataView<Real> rho0_, mass_;
-    };
-
-  protected:
-    WeaklyCompressibleMultiSpecies &fluid_mixture;
-    ConstantArray<Real> *ca_inv_rho0_list_;
-    DiscreteVariable<Real> *dv_Y_list_;
-    DiscreteVariable<Real> *dv_rho0_, *dv_mass_;
-};
-
 template <typename FluidMixtureType>
-class PrescribedReferenceDensity : public ReturnFunction<Real>
+class ConstantMixtureFraction : public ReturnFunction<Real>
 {
     using EosKernel = typename FluidMixtureType::EosKernel;
 
   public:
-    PrescribedReferenceDensity(
+    ConstantMixtureFraction(
         BaseParticles *base_particles,
         FluidMixtureType &fluid_mixture, StdVec<Real> mixture_fractions)
         : fluid_mixture_(fluid_mixture),
-          ca_mixture_fractions_("ConstantMixtureFractions", mixture_fractions),
-          dv_rho0_(fluid_mixture.dvReferenceDensity()),
-          dv_Vol_ref_(base_particles->getVariableByName<Real>(
-              "VolumetricMeasureRef")),
-          dv_mass_(base_particles->getVariableByName<Real>("Mass"))
+          ca_mixture_fractions_("ConstantMixtureFractions", mixture_fractions)
     {
         if (fluid_mixture.NumberOfMixtures() != mixture_fractions.size())
         {
@@ -109,7 +56,7 @@ class PrescribedReferenceDensity : public ReturnFunction<Real>
             exit(1);
         }
     };
-    ~PrescribedReferenceDensity() {};
+    ~ConstantMixtureFraction() {};
 
     class ComputingKernel
     {
@@ -117,27 +64,62 @@ class PrescribedReferenceDensity : public ReturnFunction<Real>
         template <class ExecutionPolicy, class EncloserType>
         ComputingKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser)
             : eos_(ex_policy, encloser.fluid_mixture_),
-              mixture_fractions_(encloser.ca_mixture_fractions_.DelegatedArrayView(ex_policy)),
+              mixture_fractions_(
+                  encloser.ca_mixture_fractions_.DelegatedArrayView(ex_policy)){};
+
+        void operator()(size_t index_i)
+        {
+            eos_.setMixtureFractions(index_i, mixture_fractions_);
+        };
+
+      protected:
+        EosKernel eos_;
+        ArrayView<Real> mixture_fractions_;
+    };
+
+  protected:
+    FluidMixtureType &fluid_mixture_;
+    ConstantArray<Real> ca_mixture_fractions_;
+};
+
+template <typename FluidMixtureType>
+class UpdateReferenceDensity : public ReturnFunction<Real>
+{
+    using EosKernel = typename FluidMixtureType::EosKernel;
+
+  public:
+    UpdateReferenceDensity(
+        BaseParticles *base_particles, FluidMixtureType &fluid_mixture)
+        : fluid_mixture_(fluid_mixture),
+          dv_rho0_(fluid_mixture.dvReferenceDensity()),
+          dv_Vol_ref_(base_particles->getVariableByName<Real>(
+              "VolumetricMeasureRef")),
+          dv_mass_(base_particles->getVariableByName<Real>("Mass")) {};
+    ~UpdateReferenceDensity() {};
+
+    class ComputingKernel
+    {
+      public:
+        template <class ExecutionPolicy, class EncloserType>
+        ComputingKernel(const ExecutionPolicy &ex_policy, EncloserType &encloser)
+            : eos_(ex_policy, encloser.fluid_mixture_),
               rho0_(encloser.dv_rho0_->DelegatedDataView(ex_policy)),
               Vol_ref_(encloser.dv_Vol_ref_->DelegatedDataView(ex_policy)),
               mass_(encloser.dv_mass_->DelegatedDataView(ex_policy)){};
 
         void operator()(size_t index_i)
         {
-            eos_.setMixtureFractions(index_i, mixture_fractions_);
             rho0_[index_i] = eos_.computeReferenceDensity(index_i);
             mass_[index_i] = rho0_[index_i] * Vol_ref_[index_i];
         };
 
       protected:
         EosKernel eos_;
-        ArrayView<Real> mixture_fractions_;
         DataView<Real> rho0_, Vol_ref_, mass_;
     };
 
   protected:
     FluidMixtureType &fluid_mixture_;
-    ConstantArray<Real> ca_mixture_fractions_;
     DiscreteVariable<Real> *dv_rho0_, *dv_Vol_ref_, *dv_mass_;
 };
 } // namespace fluid_dynamics
