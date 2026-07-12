1f6e127246b8ea07ca2be5706c3d48b57c72beb9
Jasmine969 <1337245114@qq.com>
2026-07-12 22:42:57 +0800

    :bug: Fix SYCL particle-sorting failures caused by excessive register usage on Intel GPU devices

--- src/src_sycl/shared/common/algorithm_primitive_sycl.cpp
+++ src/src_sycl/shared/common/algorithm_primitive_sycl.cpp
@@ -2,7 +2,7 @@
 
 #include "sphinxsys_variable_sycl.hpp"
 
-#ifdef SPHINXSYS_USE_ONEDPL_SORTING
+#if SPHINXSYS_USE_ONEDPL_SORTING
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/execution>
 #endif
@@ -15,9 +15,9 @@ void RadixSort::sort(const ParallelDevicePolicy &ex_policy, UnsignedInt size, Un
     UnsignedInt *index_permutation = dv_index_permutation_->DelegatedData(ex_policy);
     UnsignedInt *begin = dv_sequence_->DelegatedData(ex_policy) + start_index;
 
-#ifndef SPHINXSYS_USE_ONEDPL_SORTING
+#if !SPHINXSYS_USE_ONEDPL_SORTING
     auto &sycl_queue = execution_instance.getQueue();
-    device_radix_sorting.sort_by_key(begin, index_permutation, size, sycl_queue, sycl_queue.getWorkGroupSize(), 4).wait();
+    device_radix_sorting.sort_by_key(begin, index_permutation, size, sycl_queue, execution_instance.getWorkGroupSize(), 4).wait();
 #else
     oneapi::dpl::sort_by_key(oneapi::dpl::execution::make_device_policy(execution_instance.getQueue()),
                              begin, begin + size, index_permutation);
