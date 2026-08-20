aedb891a1840f888e6bf8756b08fa11bf47f3ac5
crb116923 <ruibin.cao@tum.de>
2026-08-20 21:36:44 +0800

    Fix formatting in J2Plasticity constructor initialization

--- src/shared/materials/general_continuum.cpp
+++ src/shared/materials/general_continuum.cpp
@@ -106,8 +106,8 @@ J2Plasticity::J2Plasticity(Real rho0, Real c0, Real youngs_modulus, Real poisson
                            Real yield_stress, Real hardening_modulus,const DuctileDamageParameters &damage_parameters)
     : GeneralContinuum(rho0, c0, youngs_modulus, poisson_ratio),
       yield_stress_(yield_stress), hardening_modulus_(hardening_modulus),
-      dv_hardening_factor_(nullptr), dv_intact_factor_(nullptr), dv_p_(nullptr),dv_damage_(nullptr),
-      failure_tension_(0.02 * youngs_modulus),damage_parameters_(damage_parameters)
+      dv_hardening_factor_(nullptr), dv_intact_factor_(nullptr), dv_p_(nullptr),
+      failure_tension_(0.02 * youngs_modulus),dv_damage_(nullptr),damage_parameters_(damage_parameters)
 {
     material_type_name_ = "J2Plasticity";
     if (damage_parameters_.enabled_)
